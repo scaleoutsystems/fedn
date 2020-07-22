11162489aec44716576a48ccea523552207e7a34
Morgan Ekmefjord <morgan@async.se>
2020-07-22 14:12:12 +0200

    Combiner can now notify of its existance and assign a configuration context for execution. - CombinerConfiguration allow for a context of execution to be set. - Combiner allow for tracking combiner state across execution of contexts.

--- components/discovery/combiner/viewset.py
+++ components/discovery/combiner/viewset.py
@@ -1,4 +1,4 @@
-from .models import Combiner
+from .models import Combiner, CombinerConfiguration
 
 app_name = 'combiner'
 
@@ -7,13 +7,28 @@ from rest_framework import routers, serializers, viewsets
 
 
 # Serializers define the API representation.
-class CombinerSerializer(serializers.HyperlinkedModelSerializer):
+class CombinerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Combiner
-        fields = ['name', 'status', 'user',]
+        fields = ['name', 'status', 'user','host','port']
 
 
 # ViewSets define the view behavior.
 class CombinerViewSet(viewsets.ModelViewSet):
     queryset = Combiner.objects.all()
     serializer_class = CombinerSerializer
+    lookup_field = 'name'
+
+
+class CombinerConfigurationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CombinerConfiguration
+        fields = ['combiner', 'timeout','rounds','clients_required','seed', 'updated_at','created_at', 'status', ]
+
+
+# ViewSets define the view behavior.
+class CombinerConfigurationViewSet(viewsets.ModelViewSet):
+    queryset = CombinerConfiguration.objects.all()
+    serializer_class = CombinerConfigurationSerializer
+    lookup_field = 'combiner__name'
+
