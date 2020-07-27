35ca58323c2f7f842d7fb3e5810a7d2f17a55d09
Morgan Ekmefjord <morgan@async.se>
2020-07-27 22:58:13 +0200

    - Introducing user friendly view for combiner configuration changes.

--- components/discovery/combiner/urls.py
+++ components/discovery/combiner/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'combiners'
@@ -6,9 +7,9 @@ app_name = 'combiners'
 urlpatterns = [
     path('', views.index, name='index'),
     path('combiners/', views.combiner, name='combiner'),
-    path('combiners/<combiner>/', views.details, name='details'),
     path('combiners/<combiner>/start', views.start, name='start'),
     path('combiners/<combiner>/stop', views.stop, name='stop'),
     path('combiners/<combiner>/snapshot', views.snapshot, name='snapshot'),
-    path('combiners/<combiner>/configure', views.configure , name='configure'),
+    path('combiners/<combiner>/configure', views.configure, name='configure'),
+path('combiners/<combiner>/', views.details, name='details'),
 ]
