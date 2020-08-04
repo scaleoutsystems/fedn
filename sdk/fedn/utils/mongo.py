fc8ba94c52a11c5aa2944928dbf4cf8da6ecc837
Andreas Hellander <andreas@emp-235-28.eduroam.uu.se>
2020-08-04 12:46:51 +0200

    Bugfixes

--- sdk/fedn/utils/mongo.py
+++ sdk/fedn/utils/mongo.py
@@ -4,16 +4,16 @@ import os
 
 def get_mongo_config():
 	config = {
-		'username': os.environ['MDBUSR']
-		'password': os.environ['MDBPWD']
-		'host': 'mongo'
-		'port': 27017
+		'username':os.environ['MDBUSR'],
+		'password':os.environ['MDBPWD'],
+		'host':'mongo',
+		'port':27017,
 	}
 	return config 
 
 
 def connect_to_mongodb():
-	config = get_mongo_config()
+    config = get_mongo_config()
     try: 
         mc = pymongo.MongoClient(**config)
         # This is so that we check that the connection is live
