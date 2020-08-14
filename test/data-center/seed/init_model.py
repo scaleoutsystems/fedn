e5f7af140ed3af67a64e0f1d1da1287f2a2fa44b
Salman Toor <Salman.Toor@gmail.com>
2020-08-14 15:21:55 +0000

    Fixies related to data-center example.

--- test/data-center/seed/init_model.py
+++ test/data-center/seed/init_model.py
@@ -9,7 +9,6 @@ from sklearn.metrics import mean_squared_error
 import requests
 import os
 
-from scaleout.project import Project
 
 import tempfile
 
@@ -38,22 +37,23 @@ if __name__ == '__main__':
 	
         # Create a seed model and push to Minio
         model = create_seed_model()
-        fod, outfile_name = tempfile.mkstemp(suffix='.h5')
+        outfile_name = "9908a112-c861-4cb1-a25d-775153e5543"
+        #fod, outfile_name = tempfile.mkstemp(suffix='.h5')
         model.save(outfile_name)
 
-        project = Project()
-        from scaleout.repository.helpers import get_repository
+        #project = Project()
+        #from scaleout.repository.helpers import get_repository
 
-        repo_config = {'storage_access_key': 'minio',
-                                   'storage_secret_key': 'minio123',
-                                   'storage_bucket': 'models',
-                                   'storage_secure_mode': False,
-                                   'storage_hostname': 'minio',
-                                   'storage_port': 9000}
-        storage = get_repository(repo_config)
+        #repo_config = {'storage_access_key': 'minio',
+        #                           'storage_secret_key': 'minio123',
+        #                           'storage_bucket': 'models',
+        #                           'storage_secure_mode': False,
+        #                           'storage_hostname': 'minio',
+        #                           'storage_port': 9000}
+        #storage = get_repository(repo_config)
 
-        model_id = storage.set_model(outfile_name,is_file=True)
-        os.unlink(outfile_name)
-        print("Created seed model with id: {}".format(model_id)) 
+        #model_id = storage.set_model(outfile_name,is_file=True)
+        #os.unlink(outfile_name)
+        #print("Created seed model with id: {}".format(model_id)) 
         
 
--- test/data_center/combiner/seed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sleep 5
-MODEL_ID=$(python3 init_model.py | sed -n -e 's/^.*Created seed model with id: //p')
-echo "found model:" $MODEL_ID
-#sleep 15
-#echo "starting:"
-#fedn run fedavg -s $MODEL_ID -r 10
-#echo "stopping..."
