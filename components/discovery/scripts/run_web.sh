fa77434d0528c4e5a24c702d9e35e3855c2f2940
Andreas Hellander <andreas@IT-ML-andhe896.local>
2020-08-19 11:30:37 +0200

    Started on docker-compose for live deployment

--- components/discovery/scripts/run_web.sh
+++ components/discovery/scripts/run_web.sh
@@ -5,4 +5,4 @@ python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations combiner
 python3 manage.py migrate
 python3 manage.py loaddata seed.json
-python3 manage.py runserver 0.0.0.0:8080
+python3 manage.py runserver ${CONTROLLER_HOST}:${CONTROLLER_PORT}
