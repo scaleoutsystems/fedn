0a83bab211bfa920bd7c9040a9a3dc585d6b9063
Morgan Ekmefjord <morgan@scaleoutsystems.com>
2020-08-30 10:45:42 +0200

    - Introduced message for connection status checking of combiner. Ensuring combiner requirements are fullfilled on the fly. - Client - new connector to check where to be assigned. - Combiner - new connector to announc ready to be tasked. - Reducer takes over most of discoverys responsibilities.
    The benefits of this is
    - Client adhoc reassignment
    - Client failover tolerance
    - Combiner logic for client is robust and can handle client dropouts.

--- test/mnist-multi-combiner/combiner/run.sh
+++ test/mnist-multi-combiner/combiner/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fedn run combiner -d discovery -p 8080 -t e9a3cb4c5eaff546eec33ff68a7fbe232b68a192 -h combiner -i 12080 -n combiner
+fedn run combiner -d reducer -p 8090 -t e9a3cb4c5eaff546eec33ff68a7fbe232b68a192 -h combiner -i 12080 -n combiner
