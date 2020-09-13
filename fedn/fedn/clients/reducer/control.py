cfc7532d01ec78e5b80db921cad0b8c8168e417d
Andreas Hellander <andreas.hellander@gmail.com>
2020-09-13 22:37:30 +0200

    Wip updating recuder API

--- fedn/fedn/clients/reducer/control.py
+++ fedn/fedn/clients/reducer/control.py
@@ -2,61 +2,73 @@ from .state import ReducerState
 import copy
 
 
+class Model:
+    """ (DB) representation of a global model. """ 
+    def __init__(self):
+
+        self.model_id = None
+        self.version = ""
+        self.parent = ""
+
+
 class ReducerControl:
 
     def __init__(self):
         self.__state = ReducerState.idle
         self.combiners = []
-        # TODO: Store in DB 
-        self.model_id = None
 
-    def get_model_id(self):
+        # TODO: Use DB / Eth
+        # models should be an immutable, ordered chain of global models
+        self.models = []
+
+    def get_latest_model(self):
         # TODO: get single point of thruth from DB / Eth backend
         return self.model_id
 
-    def set_model_id(self,model_id):
+    def commit(self,model_id):
+        """ Commit a model. This establishes this model as the lastest consensus model. """
+
         # TODO: post to DB backend
+        # TODO: Refactor into configurable
         self.model_id = model_id 
+
 
     def round(self,config):
         """ """
 
         # TODO: Set / update reducer states and such
 
-        # 1. Tell combiners to execute the compute plan / update the combiner model
+        # 1. Trigger Combiner nodes to compute an update, starting from the latest consensus model
         combiner_config = copy.deepcopy(config)
         combiner_config['rounds'] = 1
-        combiner_config['task'] = ''
+        combiner_config['task'] = 'training'
 
-        print("REDUCER: STARTING COMBINERS", flush=True)
         for combiner in self.combiners:
-            print("REDUCER: STARTING {}".format(combiner.name), flush=True)
             combiner.start(combiner_config)
-        print("REDUCER: STARTED {} COMBINERS".format(len(self.combiners), flush=True))
 
         # 2. Reducer protocol - a single global model is formed from the combiner local models
         self.resolve()
     
-        # 3. Trigger validation round 
+        # 3. Trigger Combiner nodes to execute a validation round for the model
         combiner_config = copy.deepcopy(config)
+        combiner_config['model_id'] = self.get_latest_model()
         combiner_config['task'] = 'validation'
         for combiner in self.combiners:
-            combiner_config = copy.deepcopy(config)
-            combiner_config['task'] = 'validation'
             combiner.start(combiner_config)
 
 
-    def spread_model(self,model_id):
+    def sync_combiners(self,model_id):
         """ 
-            Spread the current consensus model to all combiner nodes. 
-            After sucessful execution, all active combiners
+            Spread the current consensus model to all active combiner nodes. 
+            After execution all active combiners
             should be configured with identical model state. 
         """
         for combiner in self.combiners:
             response = combiner.set_model_id(model_id)
-            print("REDUCER_CONTROL: Setting model_ids: {}".format(response),flush=True)
 
     def instruct(self, config):
+        """ Main entrypoint, starts the control flow based on user-provided config (see Reducer class). """
+
         if self.__state == ReducerState.instructing:
             print("Already set in INSTRUCTING state", flush=True)
             return
@@ -64,9 +76,9 @@ class ReducerControl:
         self.__state = ReducerState.instructing
 
         # TODO - move seeding from config to explicit step, use Reducer REST API reducer/seed/... ?
-        if not self.get_model_id():
-            self.set_model_id(config['model_id'])
-            self.spread_model(self.get_model_id())
+        if not self.get_latest_model():
+            self.commit(config['model_id'])
+            self.sync_combiners(self.get_latest_model())
 
         for round in range(config['rounds']): 
             self.round(config)
@@ -88,18 +100,18 @@ class ReducerControl:
         ahead = []
         while len(ahead) < len(self.combiners):
           for combiner in self.combiners:
-            model_id = combiner.get_model_id()
-            if model_id != self.get_model_id():
+            model_id = combiner.get_latest_model()
+            if model_id != self.get_latest_model():
                 ahead.append(model_id)
 
         # 2. Aggregate models 
         # TODO: Do this with FedAvg strategy - we should delegate to the combiners to do this. 
-        # For now we elect one combiner local model by random sampling and proceed with that one. 
+        # For now we elect one combiner model update by random sampling and proceed with that one. 
         model_id = self.reduce_random(ahead)
 
         # 3. Propagate the new consensus model in the network
         self.spread_model(model_id)
-        self.set_model_id(model_id)
+        self.commit(model_id)
 
     def monitor(self, config=None):
         if self.__state == ReducerState.monitoring:
