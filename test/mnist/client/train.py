318080e3ce68ddf92c0548b6edfa3daa33bf111c
Andreas Hellander <andreas.hellander@gmail.com>
2020-09-24 15:07:10 +0200

    hack to make mnist clients use the same data partition in subsequent rounds

--- test/mnist/client/train.py
+++ test/mnist/client/train.py
@@ -8,6 +8,7 @@ import pickle
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 from read_data import read_data
+import os   
 
 
 def train(model,data,sample_fraction):
@@ -21,8 +22,28 @@ def train(model,data,sample_fraction):
     img_rows, img_cols = 28, 28
 
     # The data, split between train and test sets
-    (x_train, y_train, classes) = read_data(data,sample_fraction=sample_fraction)
-    
+    try:
+        with open('mnist_data/x.pyb','rb') as fh:
+            x_train=pickle.loads(fh.read())
+        with open('mnist_data/y.pyb','rb') as fh:
+            y_train=pickle.loads(fh.read())
+        with open('mnist_data/classes.pyb','rb') as fh:
+            classes=pickle.loads(fh.read())
+    except:
+        (x_train, y_train, classes) = read_data(data,sample_fraction=sample_fraction)
+
+    # This is a hack for scalability studies
+    try:
+        os.mkdir('mnist_data')
+        with open('mnist_data/x.pyb','wb') as fh:
+            fh.write(pickle.dumps(x_train))
+        with open('mnist_data/y.pyb','wb') as fh:
+            fh.write(pickle.dumps(y_train))
+        with open('mnist_data/classes.pyb','wb') as fh:
+            fh.write(pickle.dumps(classes))
+    except os.FileExistsError:
+        pass
+
     model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
 
     print("-- TRAINING COMPLETED --")
