d59674cfba2a4f90d99e03c2a27873e06727cef0
Morgan Ekmefjord <morgan@scaleoutsystems.com>
2020-10-09 21:56:09 +0200

    Removing old discovery node.

--- components/discovery/scripts/run_web.sh
+++
@@ -1,8 +0,0 @@
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
-sleep 5
-python3 -m pip install -r requirements.txt
-python3 manage.py makemigrations combiner
-python3 manage.py migrate
-python3 manage.py loaddata seed.json
-python3 manage.py runserver '0.0.0.0':${CONTROLLER_PORT}
