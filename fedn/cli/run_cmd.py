1d471048eae86ec1ac75a8d70af23220a10e6909
Andreas Hellander <andreas.hellander@gmail.com>
2020-11-10 21:25:21 +0100

    Support combiner and client settings via yaml config file

--- fedn/cli/run_cmd.py
+++ fedn/cli/run_cmd.py
@@ -62,9 +62,9 @@ def run_cmd(ctx):
 
 
 @run_cmd.command('client')
-@click.option('-d', '--discoverhost', required=True)
-@click.option('-p', '--discoverport', required=True)
-@click.option('-t', '--token', required=True)
+@click.option('-d', '--discoverhost', required=False)
+@click.option('-p', '--discoverport', required=False)
+@click.option('-t', '--token', required=False)
 @click.option('-n', '--name', required=False, default=str(uuid.uuid4()))
 @click.option('-i', '--client_id', required=False)
 @click.option('-r', '--remote', required=False, default=True, help='Enable remote configured execution context')
@@ -72,16 +72,28 @@ def run_cmd(ctx):
 @click.option('-s', '--secure', required=False, default=True)
 @click.option('-v', '--preshared-cert', required=False, default=False)
 @click.option('-v', '--verify-cert', required=False, default=False)
+@click.option('-in', '--init', required=False, default=None, help='Set to a filename to (re)init client from file state.')
 @click.pass_context
 def client_cmd(ctx, discoverhost, discoverport, token, name, client_id, remote, dry_run, secure, preshared_cert,
-               verify_cert):
-    if name == None:
-        import uuid
-        name = str(uuid.uuid4())
+               verify_cert,init):
 
     config = {'discover_host': discoverhost, 'discover_port': discoverport, 'token': token, 'name': name,
               'client_id': client_id, 'remote_compute_context': remote, 'dry_run': dry_run, 'secure': secure,
-              'preshared_cert': preshared_cert, 'verify_cert': verify_cert}
+              'preshared_cert': preshared_cert, 'verify_cert': verify_cert,'init':init}
+
+    if config['init']:
+        with open(config['init'], 'r') as file:
+            try:
+                settings = dict(yaml.safe_load(file))
+            except yaml.YAMLError as e:
+                print('Failed to read config from settings file, exiting.',flush=True)
+                raise(e)
+
+        # Read/overide settings from config file
+        if 'controller' in settings:
+            reducer_config = settings['controller']
+            for key,val in reducer_config.items():
+                config[key] = val
 
     from fedn.client import Client
     client = Client(config)
@@ -91,8 +103,8 @@ def client_cmd(ctx, discoverhost, discoverport, token, name, client_id, remote, 
 @run_cmd.command('reducer')
 @click.option('-d', '--discoverhost', required=False)
 @click.option('-p', '--discoverport', required=False)
-@click.option('-t', '--token', required=True)
-@click.option('-n', '--name', required=False, default=None)
+@click.option('-t', '--token', required=False,default="reducer_token")
+@click.option('-n', '--name', required=False, default=str(uuid.uuid4()))
 @click.option('-i', '--init', required=False, default=None, help='Set to a filename to (re)init reducer from file state.')
 @click.pass_context
 def reducer_cmd(ctx, discoverhost, discoverport, token, name, init):
@@ -154,18 +166,34 @@ def reducer_cmd(ctx, discoverhost, discoverport, token, name, init):
     reducer.run()
 
 @run_cmd.command('combiner')
-@click.option('-d', '--discoverhost', required=True)
-@click.option('-p', '--discoverport', required=True)
-@click.option('-t', '--token', required=True)
-@click.option('-n', '--name', required=False, default=None)
+@click.option('-d', '--discoverhost', required=False)
+@click.option('-p', '--discoverport', required=False)
+@click.option('-t', '--token', required=False)
+@click.option('-n', '--name', required=False, default=str(uuid.uuid4()))
 @click.option('-h', '--hostname', required=True)
 @click.option('-i', '--port', required=True)
 @click.option('-s', '--secure', required=False, default=True)
 @click.option('-c', '--max_clients', required=False, default=8)
+@click.option('-in', '--init', required=False, default=None, help='Set to a filename to (re)init combiner from file state.')
 @click.pass_context
-def combiner_cmd(ctx, discoverhost, discoverport, token, name, hostname, port, secure, max_clients):
+def combiner_cmd(ctx, discoverhost, discoverport, token, name, hostname, port, secure, max_clients,init):
     config = {'discover_host': discoverhost, 'discover_port': discoverport, 'token': token, 'myhost': hostname,
-              'myport': port, 'myname': name, 'secure': secure, 'max_clients': max_clients}
+              'myport': port, 'myname': name, 'secure': secure, 'max_clients': max_clients,'init':init}
+
+    if config['init']:
+        with open(config['init'], 'r') as file:
+            try:
+                settings = dict(yaml.safe_load(file))
+            except yaml.YAMLError as e:
+                print('Failed to read config from settings file, exiting.',flush=True)
+                raise(e)
+
+        # Read/overide settings from config file
+        if 'controller' in settings:
+            reducer_config = settings['controller']
+            for key,val in reducer_config.items():
+                config[key] = val
+
 
     from fedn.combiner import Combiner
     combiner = Combiner(config)
