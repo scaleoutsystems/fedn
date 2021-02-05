d616a4028fe4eedb5eabf19420541613f4ad3362
Andreas Hellander <andreas.hellander@gmail.com>
2021-02-05 09:11:29 +0100

    Added missing files

---
+++ test/mnist/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='client',
+    packages=find_packages(),
+    description='FEDn client for MNIST example.',
+    author='Scaleout Systems AB',
+    license='',
+)
