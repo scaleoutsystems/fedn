0ae910bb15de676b7365773d4dd22d25406921f3
mattiasakesson <ma10s@hotmail.com>
2021-02-07 23:21:28 +0100

    update

--- fedn/fedn/utils/pytorchmodel.py
+++ fedn/fedn/utils/pytorchmodel.py
@@ -17,17 +17,20 @@ class PytorchModelHelper(HelperBase):
 
 
     def get_tmp_path(self):
-        _ , path = tempfile.mkstemp(suffix='.npz')
+        fd , path = tempfile.mkstemp(suffix='.npz')
+        os.close(fd)
         return path
 
     def save_model(self, weights_dict, path=None):
         if not path:
             path = self.get_tmp_path()
-        print("we are saving")
+        print("we are saving: ", path)
+        print("weights_dict type: ", type(weights_dict))
         np.savez_compressed(path, **weights_dict)
         return path
 
     def load_model(self, path="weights.npz"):
+        print("load model: ", path)
         b = np.load(path)
         weights_np = OrderedDict()
         for i in b.files:
