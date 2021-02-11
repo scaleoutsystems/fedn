9d253e19b555d0772417b6a137a2666f426415b8
mattiasakesson <ma10s@hotmail.com>
2021-02-11 10:09:32 +0100

    bug fixes and remove unnecessary  depedencies

--- test/pytorch_mnist/client/init_model.py
+++ test/pytorch_mnist/client/init_model.py
@@ -1,12 +1,13 @@
-#from fedn.utils.pytorchmodel import PytorchModelHelper
+from fedn.utils.pytorchhelper import PytorchHelper
 from models.mnist_pytorch_model import create_seed_model
-import torch
+
+
 
 if __name__ == '__main__':
 
 	# Create a seed model and push to Minio
 	model, _, _ = create_seed_model()
-	outfile_name = "seed.pth"
-	torch.save(model.state_dict(), outfile_name)
-	#helper = PytorchModelHelper()
-	#helper.save_model(model.state_dict(), outfile_name)
+	outfile_name = "../seed/seed.npz"
+	helper = PytorchHelper()
+	helper.save_model(model.state_dict(), outfile_name)
+
