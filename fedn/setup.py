be4688e3eea5f5c7c60023a875ddab6812621ff3
Andreas Hellander <andreas.hellander@gmail.com>
2021-05-18 16:18:53 +0200

    Bump version number

--- fedn/setup.py
+++ fedn/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fedn',
-    version='0.2.2',
+    version='0.2.3',
     description="""Scaleout Federated Learning""",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
