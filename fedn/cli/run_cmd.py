e0589261278127f34b68758ee74c0518e7818119
Morgan Ekmefjord <morgan@scaleoutsystems.com>
2021-06-21 15:06:12 +0200

    Updates to example with new client.yaml file expected

--- fedn/cli/run_cmd.py
+++ fedn/cli/run_cmd.py
@@ -44,9 +44,10 @@ def client_cmd(ctx, discoverhost, discoverport, token, name, client_id, remote, 
         with open(config['init'], 'r') as file:
             try:
                 settings = dict(yaml.safe_load(file))
-            except yaml.YAMLError as e:
+            except (TypeError, yaml.YAMLError) as e:
                 print('Failed to read config from settings file, exiting.',flush=True)
-                raise(e)
+                return
+                #raise(e)
 
         # Read/overide settings from config file
         if 'controller' in settings:
