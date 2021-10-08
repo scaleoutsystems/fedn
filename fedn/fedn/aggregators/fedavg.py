f7a2dddc0a52b5c2906088d8c4ee747e994a80c8
Andreas Hellander <andreas.hellander@gmail.com>
2021-10-08 14:27:30 +0200

    Added some docstrings

--- fedn/fedn/aggregators/fedavg.py
+++ fedn/fedn/aggregators/fedavg.py
@@ -30,7 +30,11 @@ class FEDAVGCombiner:
 
     def on_model_update(self, model_id):
         """ Callback when a new model update is recieved from a client.
+            Performs (optional) pre-processing and the puts the update id
+            on the aggregation queue. 
         
+        :param model_id: ID of model update (str)
+        :return:
         """
         try:
             self.server.report_status("AGGREGATOR: callback received model {}".format(model_id),
@@ -45,7 +49,10 @@ class FEDAVGCombiner:
 
     def on_model_validation(self, validation):
         """ Callback when a new model validation is recieved from a client. 
-        
+
+        :param validation: Dict containing validation data sent by client. 
+                           Must be valid JSON.   
+        :return:
         """
 
         model_id = validation.model_id
@@ -62,6 +69,13 @@ class FEDAVGCombiner:
     def combine_models(self, nr_expected_models=None, nr_required_models=1, helper=None, timeout=180):
         """ Compute a running average of model updates.
 
+        :param nr_expected_models: The number of updates expected in this round.
+        :param nr_required_models: The number of updates needed to a valid round.
+        :param helper: An instance of the ML framework specific helper
+        :param timeout: The maximum time waiting for model updates before returning 
+                        the aggregated model. 
+                    
+        :return model,data: Tuple with the aggregated model and the performance metadata.
         """
 
         import time
