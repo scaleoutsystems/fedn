127aa7260796e3e5ea081b916332e53d891e6ccf
Marco Capuccini <marco@scaleoutsystems.com>
2022-03-09 11:45:00 +0100

    Move README from MD to RST format (#370)

--- .devcontainer/bin/init_venv.sh
+++ .devcontainer/bin/init_venv.sh
@@ -6,4 +6,8 @@ python -m venv .venv
 
 # Pip deps
 .venv/bin/pip install --upgrade pip
+.venv/bin/pip install \
+    sphinx==4.4.0 \
+    sphinx_press_theme==0.8.0 \
+    sphinx-autobuild==2021.3.14
 .venv/bin/pip install -e fedn
