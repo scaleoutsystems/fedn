ae07878fd74e87ba3b13e7dbd2624185e5fa5839
Marco Capuccini <marco@scaleoutsystems.com>
2022-05-20 16:13:07 +0200

    Update process.py

--- fedn/fedn/utils/process.py
+++ fedn/fedn/utils/process.py
@@ -10,7 +10,7 @@ def run_process(args, cwd):
     :param args:
     :param cwd:
     """
-    status = subprocess.Popen(args, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    status = subprocess.Popen(args, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
     # print(status)
     def check_io():
