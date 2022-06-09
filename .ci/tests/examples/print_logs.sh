ccce1b69e2890d64549492b5fb73066df8764fd1
mcapuccini <marco@scaleoutsystems.com>
2022-06-09 08:15:36 +0000

    fix

--- .ci/tests/examples/print_logs.sh
+++ .ci/tests/examples/print_logs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo "Minio logs"
-docker logs ${PWD}_minio_1
+docker logs ${PWD#*/}_minio_1
 
 echo "Mongo logs"
-docker logs ${PWD}_mongo_1
+docker logs ${PWD#*/}_mongo_1
 
 echo "Reducer logs"
-docker logs ${PWD}_reducer_1
+docker logs ${PWD#*/}_reducer_1
 
 echo "Combiner logs"
-docker logs ${PWD}_combiner_1
+docker logs ${PWD#*/}_combiner_1
           
 echo "Client 1 logs"
-docker logs ${PWD}_client_1
+docker logs ${PWD#*/}_client_1
 
 echo "Client 2 logs"
-docker logs ${PWD}_client_2
+docker logs ${PWD#*/}_client_2
