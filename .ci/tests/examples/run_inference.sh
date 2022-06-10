30515507da0e0bcee464c7201edbf86cb41c38ea
mcapuccini <marco@scaleoutsystems.com>
2022-06-10 09:43:26 +0000

    fix

--- .ci/tests/examples/run_inference.sh
+++ .ci/tests/examples/run_inference.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -e
+
 >&2 echo "Run inference"
 curl -k -X POST https://localhost:8090/infer
 
 >&2 echo "Checking inference success"
 ".$example/bin/python" ../../.ci/tests/examples/inference_test.py
+>&2 echo "Test completed successfully"
