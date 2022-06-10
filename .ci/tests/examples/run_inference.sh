8f84f3ce522cc81b7e4959343b38e563592276f6
mcapuccini <marco@scaleoutsystems.com>
2022-06-10 10:01:43 +0000

    fix

--- .ci/tests/examples/run_inference.sh
+++ .ci/tests/examples/run_inference.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -e
 
+# Parse example name
+if [ "$#" -ne 1 ]; then
+    >&2 echo "Wrong number of arguments (usage: run_infrence.sh <example-name>)"
+    exit 1
+fi
+example="$1"
+
 >&2 echo "Run inference"
+pushd "examples/$example"
 curl -k -X POST https://localhost:8090/infer
 
 >&2 echo "Checking inference success"
 ".$example/bin/python" ../../.ci/tests/examples/inference_test.py
+
 >&2 echo "Test completed successfully"
+popd
