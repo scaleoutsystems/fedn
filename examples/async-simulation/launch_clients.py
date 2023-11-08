b8444c6edb46b3038f3bcb8c5cda6faec11bb3db
Andreas Hellander <andreas.hellander@gmail.com>
2023-11-08 22:42:42 +0100

    latest

--- examples/async-simulation/launch_clients.py
+++ examples/async-simulation/launch_clients.py
@@ -3,14 +3,14 @@ import time
 
 from fedn.network.clients.client import Client
 
-DISCOVER_HOST = '34.207.219.112'
+DISCOVER_HOST = '3.80.63.103'
 DISCOVER_PORT = 8092
-N_CLIENTS = 10
+N_CLIENTS = 1
 
 config = {'discover_host': DISCOVER_HOST, 'discover_port': DISCOVER_PORT, 'token': None, 'name': 'testclient',
           'client_id': 1, 'remote_compute_context': True, 'force_ssl': False, 'dry_run': False, 'secure': False,
           'preshared_cert': False, 'verify': False, 'preferred_combiner': False,
-          'validator': False, 'trainer': True, 'init': None, 'logfile': 'test.log', 'heartbeat_interval': 2,
+          'validator': True, 'trainer': True, 'init': None, 'logfile': 'test.log', 'heartbeat_interval': 2,
           'reconnect_after_missed_heartbeat': 30}
 
 clients = []
