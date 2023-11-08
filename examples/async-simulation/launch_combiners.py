6c2a1e100f4993885e4219c3f37d2aa84a043823
Andreas Hellander <andreas.hellander@gmail.com>
2023-11-08 22:48:00 +0100

    removed script for combiners

--- examples/async-simulation/launch_combiners.py
+++
@@ -1,7 +0,0 @@
-from fedn.network.combiner.server import Combiner
-
-config = {'discover_host': 'localhost', 'discover_port': 8092, 'token': None, 'host': 'localhost',
-          'port': 12080, 'fqdn': None, 'name': 'combiner', 'secure': False, 'verify': False, 'max_clients': 10,
-          'init': None, 'aggregator': 'fedavg'}
-
-fedn_combiner = Combiner(config)
