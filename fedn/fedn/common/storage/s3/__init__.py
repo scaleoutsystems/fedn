a5ba3d075dd181db202bce1a03026a5e36a092b5
Andreas Hellander <andreas.hellander@gmail.com>
2023-12-04 15:57:53 +0100

    Fix code checks

--- fedn/fedn/common/storage/s3/__init__.py
+++ fedn/fedn/common/storage/s3/__init__.py
@@ -1,2 +1,2 @@
-""" Module handling storage of objects in S3/MinIO object storage. This functionality is used by the controller 
+""" Module handling storage of objects in S3/MinIO object storage. This functionality is used by the controller
 to store global models in the model trail in persistent storage. """
