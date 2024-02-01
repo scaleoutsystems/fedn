5f738394a635c3d2e10371b731313c735637c59d
Fredrik Wrede <fredrik@scaleoutsystems.com>
2024-02-01 14:18:25 +0100

    remove unused abstract methods

--- fedn/fedn/utils/helpers/plugins/numpyhelper.py
+++ fedn/fedn/utils/helpers/plugins/numpyhelper.py
@@ -5,12 +5,12 @@ from fedn.utils.helpers.helperbase import HelperBase
 
 
 class Helper(HelperBase):
-    """ FEDn helper class for pytorch models. """
+    """ FEDn helper class for models weights/parameters that can be transformed to numpy ndarrays. """
 
     def __init__(self):
         """ Initialize helper. """
         super().__init__()
-        self.name = "pytorchhelper"
+        self.name = "numpyhelper"   
 
     def increment_average(self, m1, m2, n, N):
         """ Update a weighted incremental average of model weights.
@@ -28,97 +28,6 @@ class Helper(HelperBase):
         """
 
         return [np.add(x, n*(y-x)/N) for x, y in zip(m1, m2)]
-
-    def add(self, m1, m2, a=1.0, b=1.0):
-        """ m1*a + m2*b
-
-        :param model: Current model weights with keys from torch state_dict.
-        :type model: OrderedDict
-        :param model_next: New model weights with keys from torch state_dict.
-        :type model_next: OrderedDict
-        :return: Incremental weighted average of model weights.
-        :rtype: OrderedDict
-        """
-
-        return [x*a+y*b for x, y in zip(m1, m2)]
-
-    def subtract(self, m1, m2, a=1.0, b=1.0):
-        """ m1*a - m2*b.
-
-        :param m1: Current model weights with keys from torch state_dict.
-        :type m1: OrderedDict
-        :param m2: New model weights with keys from torch state_dict.
-        :type m2: OrderedDict
-        :return: m1*a-m2*b
-        :rtype: OrderedDict
-        """
-        return self.add(m1, m2, a, -b)
-
-    def divide(self, m1, m2):
-        """ Subtract weights.
-
-        :param m1: Current model weights with keys from torch state_dict.
-        :type m1: OrderedDict
-        :param m2: New model weights with keys from torch state_dict.
-        :type m2: OrderedDict
-        :return: m1/m2.
-        :rtype: OrderedDict
-        """
-
-        return [np.divide(x, y) for x, y in zip(m1, m2)]
-
-    def multiply(self, m1, m2):
-        """ Multiply m1 by m2.
-
-        :param m1: Current model weights with keys from torch state_dict.
-        :type m1: OrderedDict
-        :param m2: New model weights with keys from torch state_dict.
-        :type m2: OrderedDict
-        :return: m1.*m2
-        :rtype: OrderedDict
-        """
-
-        return [np.multiply(x, y) for (x, y) in zip(m1, m2)]
-
-    def sqrt(self, m1):
-        """ Sqrt of m1, element-wise.
-
-        :param m1: Current model weights with keys from torch state_dict.
-        :type model: OrderedDict
-        :param model_next: New model weights with keys from torch state_dict.
-        :type model_next: OrderedDict
-        :return: sqrt(m1)
-        :rtype: OrderedDict
-        """
-
-        return [np.sqrt(x) for x in m1]
-
-    def power(self, m1, a):
-        """ m1 raised to the power of m2.
-
-        :param m1: Current model weights with keys from torch state_dict.
-        :type m1: OrderedDict
-        :param m2: New model weights with keys from torch state_dict.
-        :type a: float
-        :return: m1.^m2
-        :rtype: OrderedDict
-        """
-
-        return [np.power(x, a) for x in m1]
-
-    def norm(self, m):
-        """Compute the L1 norm of m. """
-        n = 0.0
-        for x in m:
-            n += np.linalg.norm(x, 1)
-        return n
-
-    def ones(self, m1, a):
-
-        res = []
-        for x in m1:
-            res.append(np.ones(np.shape(x))*a)
-        return res
 
     def save(self, weights, path=None):
         """ Serialize weights to file. The serialized model must be a single binary object.
