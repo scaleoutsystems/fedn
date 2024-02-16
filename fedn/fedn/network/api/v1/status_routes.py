565af410bb6f86ce375991ef1da400048644cc4b
Niklas <niklas@scaleoutsystems.com>
2024-02-16 15:29:18 +0100

    Added count (get/post) and /list (post) for statuses

--- fedn/fedn/network/api/v1/status_routes.py
+++ fedn/fedn/network/api/v1/status_routes.py
@@ -4,7 +4,8 @@ from flask import Blueprint, jsonify, request
 from fedn.network.storage.statestore.repositories.status_repository import \
     StatusRepository
 
-from .shared import api_version, get_typed_list_headers, get_use_typing, mdb
+from .shared import (api_version, get_post_data_to_kwargs,
+                     get_typed_list_headers, get_use_typing, mdb)
 
 bp = Blueprint("status", __name__, url_prefix=f"/api/{api_version}/statuses")
 
@@ -29,6 +30,37 @@ def get_statuses():
         return jsonify(response), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+@bp.route("/list", methods=["POST"])
+def list_statuses():
+    try:
+        limit, skip, sort_key, sort_order, use_typing = get_typed_list_headers(request.headers)
+        kwargs = get_post_data_to_kwargs(request)
+
+        statuses = status_repository.list(limit, skip, sort_key, sort_order, use_typing=use_typing, **kwargs)
+
+        result = [status.__dict__ for status in statuses["result"]] if use_typing else statuses["result"]
+
+        response = {
+            "count": statuses["count"],
+            "result": result
+        }
+
+        return jsonify(response), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@bp.route("/count", methods=["GET", "POST"])
+def statuses_count():
+    try:
+        kwargs = request.args.to_dict() if request.method == "GET" else get_post_data_to_kwargs(request)
+        count = status_repository.count(**kwargs)
+        response = count
+        return jsonify(response), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 404
 
 
 @bp.route("/<string:id>", methods=["GET"])
