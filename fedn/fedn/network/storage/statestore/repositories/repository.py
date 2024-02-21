393427c016974f7823bea70d34a527519031fa42
Niklas <niklas@scaleoutsystems.com>
2024-02-21 15:05:26 +0100

    Added doc string to repository

--- fedn/fedn/network/storage/statestore/repositories/repository.py
+++ fedn/fedn/network/storage/statestore/repositories/repository.py
@@ -15,6 +15,12 @@ class Repository(Generic[T]):
         self.collection = collection
 
     def get(self, id: str, use_typing: bool = False) -> T:
+        """Get an entity by id
+        param id: The id of the entity
+            type: str
+        param use_typing: Whether to return the entity as a typed object or as a dict
+        return: The entity
+        """
         id_obj = ObjectId(id)
         document = self.database[self.collection].find_one({'_id': id_obj})
 
@@ -33,6 +39,22 @@ class Repository(Generic[T]):
         pass
 
     def list(self, limit: int, skip: int, sort_key: str, sort_order=pymongo.DESCENDING, use_typing: bool = False, **kwargs) -> Dict[int, List[T]]:
+        """List entities
+        param limit: The maximum number of entities to return
+            type: int
+        param skip: The number of entities to skip
+            type: int
+        param sort_key: The key to sort by
+            type: str
+        param sort_order: The order to sort by
+            type: pymongo.DESCENDING | pymongo.ASCENDING
+        param use_typing: Whether to return the entities as typed objects or as dicts
+            type: bool
+        param kwargs: Additional query parameters
+            type: dict
+            example: {"key": "models"}
+        return: A dictionary with the count and the result
+        """
         cursor = self.database[self.collection].find(kwargs).sort(sort_key, sort_order).skip(skip or 0).limit(limit or 0)
 
         count = self.database[self.collection].count_documents(kwargs)
