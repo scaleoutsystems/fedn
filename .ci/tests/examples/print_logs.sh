77c3fc40b54166eef05c9c93ec7611a3d2da5c32
Fredrik Wrede <fredrik@scaleoutsystems.com>
2024-02-28 15:58:55 +0000

    remove dashboard

--- .ci/tests/examples/print_logs.sh
+++ .ci/tests/examples/print_logs.sh
@@ -5,9 +5,6 @@ docker logs "$(basename $PWD)_minio_1"
 echo "Mongo logs"
 docker logs "$(basename $PWD)_mongo_1"
 
-echo "Dashboard logs"
-docker logs "$(basename $PWD)_dashboard_1"
-
 echo "API-Server logs"
 docker logs "$(basename $PWD)_api-server_1"
 
