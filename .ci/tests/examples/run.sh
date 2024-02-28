0cf7fe26adf18a5660568a00f72c65e568fa923c
Fredrik Wrede <fredrik@scaleoutsystems.com>
2024-02-28 16:27:33 +0000

    fix

--- .ci/tests/examples/run.sh
+++ .ci/tests/examples/run.sh
@@ -23,13 +23,6 @@ docker-compose \
     -f docker-compose.override.yaml \
     up -d --build api-server combiner
 
-sleep 10
-
-docker-compose \
-    -f ../../docker-compose.yaml \
-    -f docker-compose.override.yaml \
-    up -d --build client
-
 >&2 echo "Wait for reducer to start"
 ".$example/bin/python" ../../.ci/tests/examples/wait_for.py reducer
 
