09da9238ecf3e86cc7d74ba8af3c41033d58740f
Niklas <niklas@scaleoutsystems.com>
2024-02-28 11:41:07 +0100

    removed new endpoints (find integration test failure)

--- fedn/fedn/network/api/server.py
+++ fedn/fedn/network/api/server.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify, request
-from v1.client_routes import bp as client_bp
-from v1.combiner_routes import bp as combiner_bp
-from v1.model_routes import bp as model_bp
-from v1.package_routes import bp as package_bp
-from v1.round_routes import bp as round_bp
-from v1.session_routes import bp as session_bp
-from v1.status_routes import bp as status_bp
-from v1.validation_routes import bp as validation_bp
+# from v1.client_routes import bp as client_bp
+# from v1.combiner_routes import bp as combiner_bp
+# from v1.model_routes import bp as model_bp
+# from v1.package_routes import bp as package_bp
+# from v1.round_routes import bp as round_bp
+# from v1.session_routes import bp as session_bp
+# from v1.status_routes import bp as status_bp
+# from v1.validation_routes import bp as validation_bp
 
 from fedn.common.config import (get_controller_config, get_modelstorage_config,
                                 get_network_config, get_statestore_config)
@@ -24,14 +24,14 @@ statestore.set_storage_backend(modelstorage_config)
 control = Control(statestore=statestore)
 api = API(statestore, control)
 app = Flask(__name__)
-app.register_blueprint(status_bp)
-app.register_blueprint(model_bp)
-app.register_blueprint(validation_bp)
-app.register_blueprint(client_bp)
-app.register_blueprint(package_bp)
-app.register_blueprint(session_bp)
-app.register_blueprint(combiner_bp)
-app.register_blueprint(round_bp)
+# app.register_blueprint(status_bp)
+# app.register_blueprint(model_bp)
+# app.register_blueprint(validation_bp)
+# app.register_blueprint(client_bp)
+# app.register_blueprint(package_bp)
+# app.register_blueprint(session_bp)
+# app.register_blueprint(combiner_bp)
+# app.register_blueprint(round_bp)
 
 
 @app.route("/get_model_trail", methods=["GET"])
