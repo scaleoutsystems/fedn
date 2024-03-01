8f2386997707c3cb81122cadfeea042888e738ea
Dinindu Seneviratne <dininduviduneth@gmail.com>
2024-03-01 15:27:16 +0000

    Change python command to python3 and use sudo for fedn installation :bookmark:

--- examples/mnist-pytorch/bin/init_venv.sh
+++ examples/mnist-pytorch/bin/init_venv.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Init venv
-python -m venv .mnist-pytorch
+python3 -m venv .mnist-pytorch
 
 # Pip deps
 .mnist-pytorch/bin/pip install --upgrade pip
-.mnist-pytorch/bin/pip install -e ../../fedn
+sudo .mnist-pytorch/bin/pip install -e ../../fedn
 .mnist-pytorch/bin/pip install -r requirements.txt
