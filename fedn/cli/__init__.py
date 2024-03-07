db2f4680a47bc3dd14cb6315c114f22af91fd5f0
Niklas <niklas@scaleoutsystems.com>
2024-03-07 12:15:20 +0100

    Added upload package

--- fedn/cli/__init__.py
+++ fedn/cli/__init__.py
@@ -1,3 +1,4 @@
 from .list_cmd import list_cmd  # noqa: F401
 from .main import main  # noqa: F401
 from .run_cmd import run_cmd  # noqa: F401
+from .upload_cmd import upload_cmd  # noqa: F401
---
+++ fedn/cli/upload_cmd.py
@@ -0,0 +1,48 @@
+import click
+import requests
+
+from .main import main
+from .shared import API_VERSION, CONTROLLER_DEFAULTS
+
+
+@main.group('upload')
+@click.pass_context
+def upload_cmd(ctx):
+    """
+    :param ctx:
+    """
+    pass
+
+
+
+@click.option('-h', '--host', required=True, default=CONTROLLER_DEFAULTS['host'], help='Hostname of controller (api).')
+@click.option('-i', '--port', required=True, default=CONTROLLER_DEFAULTS['port'], help='Port of controller (api).')
+@click.option('-hl', '--helper', required=True, default="numpyhelper", help='Helper type to use.')
+@click.option('-p', '--path', required=True, help='Path to package file.')
+@click.option('-n', '--name', required=False, help='Name of package.')
+@click.option('-d', '--description', required=False, help='Description of package.')
+@upload_cmd.command('package')
+@click.pass_context
+def upload_package(ctx, host: str, port: int, helper: str, path: str, name: str = None, description: str = None):
+    """
+    - Upload a package to the controller.
+    return: None
+    """
+    url = f'http://{host}:{port}/set_package'
+    headers = {}
+
+    click.echo(f'\nUploading package: {url}\n')
+
+    try:
+        with open(path, 'rb') as file:
+            response = requests.post(url, files={'file': file}, data={
+                                     'helper': helper, 'name': name, 'description': description})
+
+            if response.status_code == 200:
+                click.echo("Success! Package uploaded.")
+            else: 
+                click.echo("Something went wrong. Please try again.")
+    except FileNotFoundError:
+        click.echo(f'Error: Could not find file {path}')
+    except requests.exceptions.ConnectionError:
+        click.echo(f'Error: Could not connect to {host}:{port}')
