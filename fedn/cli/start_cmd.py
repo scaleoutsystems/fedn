0114fb98de15fd968c469eab2c1e7100288fd29a
Niklas <niklas@scaleoutsystems.com>
2024-03-07 17:02:27 +0100

    Added start session

---
+++ fedn/cli/start_cmd.py
@@ -0,0 +1,47 @@
+import json
+
+import click
+import requests
+
+from .main import main
+from .shared import API_VERSION, CONTROLLER_DEFAULTS
+
+
+@main.group('start')
+@click.pass_context
+def start_cmd(ctx):
+    """
+    :param ctx:
+    """
+    pass
+
+
+@click.option('-h', '--host', required=True, default=CONTROLLER_DEFAULTS['host'], help='Hostname of controller (api).')
+@click.option('-i', '--port', required=True, default=CONTROLLER_DEFAULTS['port'], help='Port of controller (api).')
+@click.option('-n', '--name', required=False, help='Name of session (session id).')
+@click.option('-v', '--validate', required=True, default=True, help='Validate the session. Set to False to skip validation.')
+@start_cmd.command('session')
+@click.pass_context
+def start_session(ctx, host: str, port: int, name: str = None, validate: bool = True):
+    """
+    - Start a session.
+    return: None
+    """
+    url = f'http://{host}:{port}/start_session'
+    headers = {'Content-Type': 'application/json'}
+
+    click.echo(f'\nStarting session: {url}\n')
+
+    try:
+
+        data = {'session_id': name, 'validate': validate}
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+
+        if response.status_code == 200:
+            json_data = response.json()
+            click.echo(json.dumps(json_data, indent=4))
+        else:
+            click.echo("Something went wrong. Please try again.")
+
+    except requests.exceptions.ConnectionError:
+        click.echo(f'Error: Could not connect to {host}:{port}')
--- fedn/cli/upload_cmd.py
+++ fedn/cli/upload_cmd.py
@@ -49,13 +49,10 @@ def upload_package(ctx, host: str, port: int, helper: str, path: str, name: str 
 
 @click.option('-h', '--host', required=True, default=CONTROLLER_DEFAULTS['host'], help='Hostname of controller (api).')
 @click.option('-i', '--port', required=True, default=CONTROLLER_DEFAULTS['port'], help='Port of controller (api).')
-@click.option('-hl', '--helper', required=True, default="numpyhelper", help='Helper type to use.')
 @click.option('-p', '--path', required=True, help='Path to model file.')
-@click.option('-n', '--name', required=False, help='Name of model.')
-@click.option('-d', '--description', required=False, help='Description of model.')
 @upload_cmd.command('model')
 @click.pass_context
-def upload_model(ctx, host: str, port: int, helper: str, path: str, name: str = None, description: str = None):
+def upload_model(ctx, host: str, port: int, path: str):
     """
     - Upload a model to the controller.
     return: None
@@ -67,11 +64,10 @@ def upload_model(ctx, host: str, port: int, helper: str, path: str, name: str = 
 
     try:
         with open(path, 'rb') as file:
-            response = requests.post(url, files={'file': file}, data={
-                                     'helper': helper, 'name': name, 'description': description})
+            response = requests.post(url, files={'file': file})
 
             if response.status_code == 200:
-                click.echo("Success! model uploaded.")
+                click.echo("Success! Model uploaded.")
             else: 
                 click.echo("Something went wrong. Please try again.")
     except FileNotFoundError:
