ef7378aa3d074d0a6f5a438aa38dc9e52b6b730d
Niklas <niklas@scaleoutsystems.com>
2024-03-12 16:07:38 +0100

    added helper functions to get url and token

--- fedn/cli/shared.py
+++ fedn/cli/shared.py
@@ -1,4 +1,7 @@
+import os
+
 CONTROLLER_DEFAULTS = {
+    'protocol': 'http',
     'host': 'localhost',
     'port': 8092,
     'debug': False
@@ -19,3 +22,22 @@ CLIENT_DEFAULTS = {
 }
 
 API_VERSION = 'v1'
+
+
+def get_api_url(protocol: str, host: str, port: str, endpoint: str) -> str:
+    _protocol = os.environ.get('FEDN_PROTOCOL') or protocol or CONTROLLER_DEFAULTS['protocol']
+    _host = os.environ.get('FEDN_HOST') or host or CONTROLLER_DEFAULTS['host']
+    _port = os.environ.get('FEDN_PORT') or port or CONTROLLER_DEFAULTS['port']
+
+    return f'{_protocol}://{_host}:{_port}/api/{API_VERSION}/{endpoint}'
+
+
+def get_token(token: str) -> str:
+    _token = os.environ.get("FEDN_TOKEN", token)
+
+    if _token is None:
+        return None
+
+    scheme = os.environ.get("FEDN_AUTH_SCHEME", "Bearer")
+
+    return f"{scheme} {_token}"
