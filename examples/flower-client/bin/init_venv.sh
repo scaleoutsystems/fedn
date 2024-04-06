a0ee7012937eb285a4834685e93427452e93bd22
Andreas Hellander <andreas.hellander@gmail.com>
2024-04-06 07:51:18 +0200

    Fix

--- examples/flower-client/bin/init_venv.sh
+++ examples/flower-client/bin/init_venv.sh
@@ -5,6 +5,6 @@ set -e
 python3 -m venv .flower-client
 
 # Pip deps
-.flower-example/bin/pip install --upgrade pip
-.flower-example/bin/pip install -e ../../fedn
-.flower-example/bin/pip install -r requirements.txt
+.flower-client/bin/pip install --upgrade pip
+.flower-client/bin/pip install -e ../../fedn
+.flower-client/bin/pip install -r requirements.txt
