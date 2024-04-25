149c88878dbaef38af436381e6a240d46ac56f74
Niklas <niklas@scaleoutsystems.com>
2024-04-25 13:34:36 +0200

    fedn package list - added

--- fedn/cli/__init__.py
+++ fedn/cli/__init__.py
@@ -1,4 +1,5 @@
 from .client_cmd import client_cmd  # noqa: F401
+from .combiner_cmd import combiner_cmd  # noqa: F401
 from .main import main  # noqa: F401
 from .package_cmd import package_cmd  # noqa: F401
 from .run_cmd import run_cmd  # noqa: F401
--- fedn/cli/package_cmd.py
+++ fedn/cli/package_cmd.py
@@ -2,10 +2,12 @@ import os
 import tarfile
 
 import click
+import requests
 
 from fedn.common.log_config import logger
 
 from .main import main
+from .shared import CONTROLLER_DEFAULTS, get_api_url, get_token, print_response
 
 
 @main.group('package')
@@ -39,3 +41,37 @@ def create_cmd(ctx, path, name):
     with tarfile.open(name, "w:gz") as tar:
         tar.add(path, arcname=os.path.basename(path))
         logger.info(f"Created package {name}")
+
+
+@click.option('-p', '--protocol', required=False, default=CONTROLLER_DEFAULTS['protocol'], help='Communication protocol of controller (api)')
+@click.option('-H', '--host', required=False, default=CONTROLLER_DEFAULTS['host'], help='Hostname of controller (api)')
+@click.option('-P', '--port', required=False, default=CONTROLLER_DEFAULTS['port'], help='Port of controller (api)')
+@click.option('-t', '--token', required=False, help='Authentication token')
+@click.option('--n_max', required=False, help='Number of items to list')
+@package_cmd.command('list')
+@click.pass_context
+def list_packages(ctx, protocol: str, host: str, port: str, token: str = None, n_max: int = None):
+    """
+    return:
+    - count: number of packages
+    - result: list of packages
+    """
+    url = get_api_url(protocol=protocol, host=host, port=port, endpoint='packages')
+    headers = {}
+
+    if n_max:
+        headers['X-Limit'] = n_max
+
+    _token = get_token(token)
+
+    if _token:
+        headers['Authorization'] = _token
+
+    click.echo(f'\nListing packages: {url}\n')
+    click.echo(f'Headers: {headers}')
+
+    try:
+        response = requests.get(url, headers=headers)
+        print_response(response, 'packages')
+    except requests.exceptions.ConnectionError:
+        click.echo(f'Error: Could not connect to {url}')
