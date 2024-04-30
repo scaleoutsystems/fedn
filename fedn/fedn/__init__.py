7027d20a2619aafab9f54229f1524f93c1a98812
viktorvaladi <viktor.valadi@hotmail.se>
2024-04-30 10:15:12 +0200

    move init for pyproject

--- fedn/fedn/__init__.py
+++ fedn/fedn/__init__.py
@@ -1,24 +1,3 @@
-import glob
-import os
-from os.path import basename, dirname, isfile
-
-from fedn.network.api.client import APIClient
+"""The fedn package."""
 
 # flake8: noqa
-
-
-modules = glob.glob(dirname(__file__) + "/*.py")
-__all__ = [basename(f)[:-3] for f in modules if isfile(f)
-           and not f.endswith('__init__.py')]
-
-
-_ROOT = os.path.abspath(os.path.dirname(__file__))
-
-
-def get_data(path):
-    """
-
-    :param path:
-    :return:
-    """
-    return os.path.join(_ROOT, 'data', path)
