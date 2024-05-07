8ebe64d419b85f7255483e400925565488fdbdc9
viktorvaladi <viktor.valadi@hotmail.se>
2024-05-07 17:33:38 +0200

    fix imports

--- examples/mnist-pytorch/client/validate.py
+++ examples/mnist-pytorch/client/validate.py
@@ -4,8 +4,9 @@ import sys
 import torch
 from model import load_parameters
 
-from data import load_data
 from fedn.utils.helpers.helpers import save_metrics
+
+from .data import load_data
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(dir_path))
