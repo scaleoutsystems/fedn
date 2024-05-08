fb76c474f0651b22582d79372290b33e1d831a94
Fredrik Wrede <fredrik@scaleoutsystems.com>
2024-05-08 13:22:48 +0200

    Bug/SK-841 | Fix docs build after folder refactor #602

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ import sys
 import sphinx_rtd_theme  # noqa: F401
 
 # Insert path
-sys.path.insert(0, os.path.abspath('../fedn'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # Project info
 project = 'FEDn'
