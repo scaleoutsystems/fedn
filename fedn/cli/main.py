ba333c302db1f7afed77a1a6d9554248d5f3c78f
sowmyasri <sowmya@scaleoutsystems.com>
2024-07-03 15:43:41 +0200

    added --version command for fedn package

--- fedn/cli/main.py
+++ fedn/cli/main.py
@@ -1,12 +1,17 @@
 import click
-
+import importlib.metadata
 CONTEXT_SETTINGS = dict(
     # Support -h as a shortcut for --help
     help_option_names=["-h", "--help"],
 )
 
-
+# Dynamically get the version of the package
+try:
+    version = importlib.metadata.version('fedn')
+except importlib.metadata.PackageNotFoundError:
+    version = 'unknown'
 @click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version)
 @click.pass_context
 def main(ctx):
     """:param ctx:
