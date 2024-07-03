44206e509ce45d00909f75c11bf07cb21df1373b
sowmyasri <sowmya@scaleoutsystems.com>
2024-07-03 16:39:05 +0200

    intendation error fixed

--- fedn/cli/main.py
+++ fedn/cli/main.py
@@ -1,5 +1,7 @@
-import click
 import importlib.metadata
+
+import click
+
 CONTEXT_SETTINGS = dict(
     # Support -h as a shortcut for --help
     help_option_names=["-h", "--help"],
@@ -10,10 +12,11 @@ try:
     version = importlib.metadata.version("fedn")
 except importlib.metadata.PackageNotFoundError:
     version = "unknown"
+
+
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version)
 @click.pass_context
 def main(ctx):
-    """:param ctx:
-    """
+    """:param ctx:"""
     ctx.obj = dict()
