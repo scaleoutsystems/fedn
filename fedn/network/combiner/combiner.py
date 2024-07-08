e9815a5b6cfeb929f1f7ceebf7cac73e0a4c0c6a
Stefan Hellander <stefan@scaleoutsystems.com>
2024-07-08 12:01:29 +0200

    Reverted changes in combiner.py

--- fedn/network/combiner/combiner.py
+++ fedn/network/combiner/combiner.py
@@ -12,8 +12,7 @@ from enum import Enum
 
 import fedn.network.grpc.fedn_pb2 as fedn
 import fedn.network.grpc.fedn_pb2_grpc as rpc
-from fedn.common.log_config import (logger, set_log_level_from_string,
-                                    set_log_stream)
+from fedn.common.log_config import logger, set_log_level_from_string, set_log_stream
 from fedn.network.combiner.connect import ConnectorCombiner, Status
 from fedn.network.combiner.modelservice import ModelService
 from fedn.network.combiner.roundhandler import RoundConfig, RoundHandler
@@ -702,20 +701,9 @@ class Combiner(rpc.CombinerServicer, rpc.ReducerServicer, rpc.ConnectorServicer,
     def run(self):
         """Start the server."""
         logger.info("COMBINER: {} started, ready for gRPC requests.".format(self.id))
-        
-        def handle_signal(signum, frame):
-            logger.warning(f"Received signal {signum}, shutting down gracefully.")
-            self.server.stop(0)  # Add a timeout if necessary
-            logger.warning("Server stopped.")
-            exit(0)
-
-        signal.signal(signal.SIGINT, handle_signal)  # Handle Ctrl-C
-        signal.signal(signal.SIGTERM, handle_signal)  # Handle Docker stop
-        
-        # try:
-        logger.warning("STARTING UP")
-        while True:
-            signal.pause()
-        # except (KeyboardInterrupt, SystemExit):
-        #     pass
+        try:
+            while True:
+                signal.pause()
+        except (KeyboardInterrupt, SystemExit):
+            pass
         self.server.stop()
