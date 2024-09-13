a1a8a02481467344306e51ee11b2e5737cd0eaf1
Fredrik Wrede <fredrik@scaleoutsystems.com>
2024-09-13 14:49:08 +0200

    Fix/SK-1025 | Fix torch dependencies based on arch (#701)

--- fedn/network/combiner/shared.py
+++ fedn/network/combiner/shared.py
@@ -8,6 +8,6 @@ modelstorage_config = get_modelstorage_config()
 network_id = get_network_config()
 
 statestore = MongoStateStore(network_id, statestore_config["mongo_config"])
-repository = Repository(modelstorage_config["storage_config"])
+repository = Repository(modelstorage_config["storage_config"], init_buckets=False)
 
 modelservice = ModelService()
