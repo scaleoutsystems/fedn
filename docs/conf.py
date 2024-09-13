3daa82c59fc4d21c4342b318bbb39eb7f2c77ad8
Fredrik Wrede <fredrik@scaleoutsystems.com>
2024-09-13 12:51:03 +0000

    bump

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ project = "FEDn"
 author = "Scaleout Systems AB"
 
 # The full version, including alpha/beta/rc tags
-release = "0.14.0"
+release = "0.15.0"
 
 # Add any Sphinx extension module names here, as strings
 extensions = [
