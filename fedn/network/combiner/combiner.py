b331636de73d92d33fb932e25774552f5369fedc
niklastheman <e.niklas.holmstrom@gmail.com>
2024-10-07 14:01:33 +0200

    removed logg 4 developing purpose

--- fedn/network/combiner/combiner.py
+++ fedn/network/combiner/combiner.py
@@ -594,7 +594,6 @@ class Combiner(rpc.CombinerServicer, rpc.ReducerServicer, rpc.ConnectorServicer,
         :rtype: :class:`fedn.network.grpc.fedn_pb2.Response`
         """
         logger.debug("GRPC: Received heartbeat from {}".format(heartbeat.sender.name))
-        logger.info("GRPC: Received heartbeat from {}".format(heartbeat.sender.name))
         # Update the clients dict with the last seen timestamp.
         client = heartbeat.sender
         self.__join_client(client)
