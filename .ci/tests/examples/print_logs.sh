cec0c20e40486b232213a43f67d63c611f332794
Fredrik Wrede <fredrik@scaleoutsystems.com>
2024-11-04 08:31:43 +0000

    update integration tests logs

--- .ci/tests/examples/print_logs.sh
+++ .ci/tests/examples/print_logs.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
-echo "Minio logs"
-docker logs "$(basename $PWD)-minio-1"
+service = $1
+example = $2
+helper = $3
 
-echo "Mongo logs"
-docker logs "$(basename $PWD)-mongo-1"
+if [ "$service" == "minio" ]; then
+    echo "Minio logs"
+    docker logs "$(basename $PWD)-minio-1"
+    exit 0
+fi
 
-echo "API-Server logs"
-docker logs "$(basename $PWD)-api-server-1"
+if [ "$service" == "mongo" ]; then
+    echo "Mongo logs"
+    docker logs "$(basename $PWD)-mongo-1"
+    exit 0
+fi
 
-echo "Combiner logs"
-docker logs "$(basename $PWD)-combiner-1"
-          
-echo "Client 1 logs"
-if [ "$example" == "mnist-keras" ]; then
-    docker logs "$(basename $PWD)-client-1"
-else
-    docker logs "$(basename $PWD)-client1-1"
+if [ "$service" == "api-server" ]; then
+    echo "API-Server logs"
+    docker logs "$(basename $PWD)-api-server-1"
+    exit 0
+fi
+
+if [ "$service" == "combiner" ]; then
+    echo "Reducer logs"
+    docker logs "$(basename $PWD)-combiner-1"
+    exit 0
+fi
+
+if [ "$service" == "client" ]; then
+    echo "Client 0 logs"
+    if [ "$example" == "mnist-keras" ]; then
+        docker logs "$(basename $PWD)-client-0"
+    else
+        docker logs "$(basename $PWD)-client0-1"
+    fi
+    exit 0
 fi
