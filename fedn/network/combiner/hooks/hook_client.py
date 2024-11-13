2410c3269f7be942da150484fe9bf4989ba8508f
viktorvaladi <viktor.valadi@hotmail.se>
2024-11-13 15:37:41 +0100

    change log level

--- fedn/network/combiner/hooks/hook_client.py
+++ fedn/network/combiner/hooks/hook_client.py
@@ -46,7 +46,7 @@ class CombinerHookInterface:
             return response.available_functions
         except grpc.RpcError as rpc_error:
             if rpc_error.code() == grpc.StatusCode.UNAVAILABLE:
-                logger.info("Server-functions container is unavailable; using default implementations.")
+                logger.warning("Server-functions container is unavailable; using default implementations.")
             else:
                 logger.error(f"gRPC error: {rpc_error.code().name} - {rpc_error.details()}")
             return {}
