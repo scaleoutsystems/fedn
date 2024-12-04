b7348ec59ef31d7817bf8aa45a155a965a9b52ea
benjaminastrand <benjamin01.astrand@gmail.com>
2024-12-04 17:06:47 +0100

    File to store configuration settings

---
+++ examples/cifar100/config.py
@@ -0,0 +1,9 @@
+settings = {
+    "N_CLIENTS": 5,
+    "DISCOVER_HOST": "localhost",
+    "DISCOVER_PORT": 8092,
+    "SECURE": False,
+    "VERIFY": False,
+    "ADMIN_TOKEN": None,
+    "CLIENT_TOKEN": None,
+}
