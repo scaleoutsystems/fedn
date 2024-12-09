9292e526c58f5718e29c0ea404cbc8a8e99f3993
Andreas Hellander <andreas.hellander@gmail.com>
2024-12-09 11:13:04 +0100

    clean up

--- examples/load-test/init_fedn.py
+++
@@ -1,8 +0,0 @@
-from fedn import APIClient
-
-DISCOVER_HOST = '127.0.0.1'
-DISCOVER_PORT = 8092
-
-client = APIClient(DISCOVER_HOST, DISCOVER_PORT)
-client.set_package('package.tgz', 'numpyhelper')
-client.set_initial_model('seed.npz')
