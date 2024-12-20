0fe3cfd388d3533cbee6a4d7fc222a271ce90022
niklastheman <e.niklas.holmstrom@gmail.com>
2024-12-20 16:37:54 +0100

    added models

--- fedn/network/api/v1/graphql/schema.py
+++ fedn/network/api/v1/graphql/schema.py
@@ -1,6 +1,16 @@
 import graphene
+import pymongo
 
-from fedn.network.api.v1.shared import session_store
+from fedn.network.api.v1.shared import model_store, session_store
+
+
+class ModelType(graphene.ObjectType):
+    id = graphene.String()
+    model = graphene.String()
+    name = graphene.String()
+    committed_at = graphene.DateTime()
+    session_id = graphene.String()
+    parent_model = graphene.String()
 
 
 class SessionConfigType(graphene.ObjectType):
@@ -22,25 +32,55 @@ class SessionType(graphene.ObjectType):
     name = graphene.String()
     committed_at = graphene.DateTime()
     session_config = graphene.Field(SessionConfigType)
+    model = graphene.List(ModelType)
 
     def resolve_session_config(self, info):
         return self["session_config"]
+
+    def resolve_model(self, info):
+        kwargs = {"session_id": self["session_id"]}
+        response = model_store.list(0, 0, None, sort_order=pymongo.DESCENDING, use_typing=False, **kwargs)
+        result = response["result"]
+
+        return result
 
 
 class Query(graphene.ObjectType):
     session = graphene.List(
         SessionType,
         id=graphene.String(),
+        name=graphene.String(),
     )
 
-    def resolve_session(root, info, id=None):
+    model = graphene.List(
+        ModelType,
+        id=graphene.String(),
+    )
+
+    def resolve_session(root, info, id=None, name=None):
         result = None
         if id:
             response = session_store.get(id)
             result = []
             result.append(response)
+        elif name:
+            kwargs = {"name": name}
+            response = session_store.list(0, 0, None, sort_order=pymongo.DESCENDING, use_typing=False, **kwargs)
+            result = response["result"]
         else:
             response = session_store.list(0, 0, None)
+            result = response["result"]
+
+        return result
+
+    def resolve_model(root, info, id=None):
+        result = None
+        if id:
+            response = model_store.get(id)
+            result = []
+            result.append(response)
+        else:
+            response = model_store.list(0, 0, None)
             result = response["result"]
 
         return result
