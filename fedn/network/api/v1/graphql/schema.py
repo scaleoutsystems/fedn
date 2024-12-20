bab877ef4d141b21004bbe49fedf0a4984e96eaf
niklastheman <e.niklas.holmstrom@gmail.com>
2024-12-20 15:43:24 +0100

    sessions added

--- fedn/network/api/v1/graphql/schema.py
+++ fedn/network/api/v1/graphql/schema.py
@@ -1,9 +1,30 @@
 import graphene
 
+from fedn.network.api.v1.shared import session_store
+
+
+class SessionConfigType(graphene.ObjectType):
+    aggregator = graphene.String()
+    round_timeout = graphene.Int()
+    buffer_size = graphene.Int()
+    model_id = graphene.String()
+    delete_models_storage = graphene.Boolean()
+    clients_required = graphene.Int()
+    helper_type = graphene.String()
+    validate = graphene.Boolean()
+    session_id = graphene.String()
+    model_id = graphene.String()
+
 
 class SessionType(graphene.ObjectType):
     id = graphene.String()
+    session_id = graphene.String()
     name = graphene.String()
+    committed_at = graphene.DateTime()
+    session_config = graphene.Field(SessionConfigType)
+
+    def resolve_session_config(self, info):
+        return self["session_config"]
 
 
 class Query(graphene.ObjectType):
@@ -13,14 +34,10 @@ class Query(graphene.ObjectType):
         name=graphene.String(),
     )
 
-    def resolve_hello(root, info):
-        return "Hello, GraphQL!"
-
     def resolve_session(root, info, id=None, name=None, pet_name=None):
-        return [
-            {"id": "1", "name": "Session 1"},
-            {"id": "2", "name": "Session 2"},
-        ]
+        response = session_store.list(0, 0, None)
+
+        return response["result"]
 
 
 schema = graphene.Schema(query=Query)
