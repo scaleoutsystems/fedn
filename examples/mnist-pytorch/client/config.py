5b9a17f2692d539b169dda5a193b3523a912b880
benjaminastrand <benjamin01.astrand@gmail.com>
2025-01-20 11:06:36 +0100

    Add config file

---
+++ examples/mnist-pytorch/client/config.py
@@ -0,0 +1,11 @@
+settings = {
+    "N_CLIENTS": 5,
+    "DISCOVER_HOST": "",
+    "DISCOVER_PORT": None,
+    "SECURE": True,
+    "VERIFY": True,
+    "ADMIN_TOKEN": "",
+    "CLIENT_TOKEN": "",
+    "BATCH_SIZE": 32,
+    "EPOCHS": 1,
+}
