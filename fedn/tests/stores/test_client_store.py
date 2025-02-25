24004df6b30155dc6cd3a6b220b19661e7ca4483
Carl Andersson <carl@scaleoutsystems.com>
2025-02-25 14:29:20 +0100

    Split basic database interaction into the different database types

--- fedn/tests/stores/test_client_store.py
+++ fedn/tests/stores/test_client_store.py
@@ -72,49 +72,58 @@ def options():
 
 class TestClientStore:
 
-    def test_add_get_update_delete(self, postgres_connection:DatabaseConnection, sql_connection: DatabaseConnection, mongo_connection:DatabaseConnection):
+    def test_add_get_update_delete_postgres(self, postgres_connection:DatabaseConnection):
+        self.helper_add_get_update_delete(postgres_connection)
+    
+    def test_add_get_update_delete_sqlite(self, sql_connection:DatabaseConnection):
+        self.helper_add_get_update_delete(sql_connection)
+    
+    def test_add_get_update_delete_mongo(self, mongo_connection:DatabaseConnection):
+        self.helper_add_get_update_delete(mongo_connection)
+
+    def helper_add_get_update_delete(self, db:DatabaseConnection):
         start_date = datetime.datetime(2021, 1, 4, 1, 2, 4)
         c = Client(name = "name", combiner = "combiner", combiner_preferred = "combiner_preferred", ip = "ip", status = "status", last_seen = start_date, package = "package")
-        for db in [postgres_connection, sql_connection, mongo_connection]:
-            # Add a client and check that we get the added client back
-            success, read_client1 = db.client_store.add(c)
-            assert success == True
-            assert isinstance(read_client1.client_id, str)
-            read_client1_dict = read_client1.to_dict()
-            client_id = read_client1_dict["client_id"]
-            del read_client1_dict["client_id"]
-            assert read_client1_dict == c.to_dict()
+    
+        # Add a client and check that we get the added client back
+        success, read_client1 = db.client_store.add(c)
+        assert success == True
+        assert isinstance(read_client1.client_id, str)
+        read_client1_dict = read_client1.to_dict()
+        client_id = read_client1_dict["client_id"]
+        del read_client1_dict["client_id"]
+        assert read_client1_dict == c.to_dict()
 
-            # Assert we get the same client back
-            read_client2 = db.client_store.get(client_id)
-            assert read_client2 is not None
-            assert read_client2.to_dict() == read_client1.to_dict()
-            
-            # Update the client and check that we get the updated client back
-            read_client2.name = "new_name"         
-            success, read_client3 = db.client_store.update(read_client2)
-            assert success == True
-            assert read_client3.name == "new_name"
+        # Assert we get the same client back
+        read_client2 = db.client_store.get(client_id)
+        assert read_client2 is not None
+        assert read_client2.to_dict() == read_client1.to_dict()
+        
+        # Update the client and check that we get the updated client back
+        read_client2.name = "new_name"         
+        success, read_client3 = db.client_store.update(read_client2)
+        assert success == True
+        assert read_client3.name == "new_name"
 
-            # Assert we get the same client back
-            read_client4 = db.client_store.get(client_id)
-            assert read_client4 is not None
-            assert read_client3.to_dict() == read_client4.to_dict()
+        # Assert we get the same client back
+        read_client4 = db.client_store.get(client_id)
+        assert read_client4 is not None
+        assert read_client3.to_dict() == read_client4.to_dict()
 
-            # Partial update the client and check that we get the updated client back
-            update_client = Client(client_id=client_id, combiner="new_combiner")
-            success, read_client5 = db.client_store.update(update_client)
-            assert success == True
-            assert read_client5.combiner == "new_combiner"
+        # Partial update the client and check that we get the updated client back
+        update_client = Client(client_id=client_id, combiner="new_combiner")
+        success, read_client5 = db.client_store.update(update_client)
+        assert success == True
+        assert read_client5.combiner == "new_combiner"
 
-            # Assert we get the same client back
-            read_client6 = db.client_store.get(client_id)
-            assert read_client6 is not None
-            assert read_client6.to_dict() == read_client5.to_dict()
+        # Assert we get the same client back
+        read_client6 = db.client_store.get(client_id)
+        assert read_client6 is not None
+        assert read_client6.to_dict() == read_client5.to_dict()
 
-            # Delete the client and check that it is deleted
-            success = db.client_store.delete(client_id)
-            assert success == True
+        # Delete the client and check that it is deleted
+        success = db.client_store.delete(client_id)
+        assert success == True
 
     def test_list_count(self, db_connections_with_data: list[tuple[str, DatabaseConnection]], options: list[tuple]):   
         for (name1, db_1), (name2, db_2) in zip(db_connections_with_data[1:], db_connections_with_data[:-1]):
