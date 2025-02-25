3d863c296fe91a7bd64b7409bdf34272b65c6602
carl-andersson <carl@scaleoutsystems.com>
2025-02-25 16:17:10 +0100

    Feature/SK-1359 | Unit test get (single) methods for client (#821)

--- fedn/tests/stores/test_client_store.py
+++ fedn/tests/stores/test_client_store.py
@@ -22,35 +22,45 @@ def test_clients():
         Client(client_id=str(uuid.uuid4()), name="test_client7", combiner="test_combiner2", ip="121.12.32.22", combiner_preferred="", status="test_status2", last_seen=start_date - datetime.timedelta(minutes=1), package="remote"),
     ]
 
+@pytest.fixture
+def test_client():
+    start_date = datetime.datetime(2021, 1, 4, 1, 2, 4)
+    c = Client(name = "name", combiner = "combiner", combiner_preferred = "combiner_preferred", ip = "ip", status = "status", last_seen = start_date, package = "package")
+    return c
 
 @pytest.fixture
 def db_connections_with_data(postgres_connection:DatabaseConnection, sql_connection: DatabaseConnection, mongo_connection:DatabaseConnection, test_clients):
     for c in test_clients:
-        res, msg = mongo_connection.client_store.add(c)
-        assert res == True
+        mongo_connection.client_store.add(c)
 
     for c in test_clients:
-        res, _ = postgres_connection.client_store.add(c)
-        assert res == True
+        postgres_connection.client_store.add(c)
 
     for c in test_clients:
-        res, _ = sql_connection.client_store.add(c)
-        assert res == True
+        sql_connection.client_store.add(c)
 
     yield [("postgres", postgres_connection), ("sqlite", sql_connection), ("mongo", mongo_connection)]
 
-    # TODO:Clean up
+    for c in test_clients:
+        mongo_connection.client_store.delete(c.client_id)
+    
+    for c in test_clients:
+        postgres_connection.client_store.delete(c.client_id)
+    
+    for c in test_clients:
+        sql_connection.client_store.delete(c.client_id)
 
 
 
 @pytest.fixture
 def options():
-    sorting_keys = (#None, 
-                        "name",
-                        "client_id",
-                        "last_seen",
-                        #"invalid_key"
-                        ) 
+    sorting_keys = (None,
+                    "name",
+                    "client_id",
+                    "last_seen",
+                    "ip", # None unique key 
+                    "invalid_key"
+                    ) 
     limits = (None, 0, 1, 2, 99)
     skips = (None, 0, 1, 2, 99)
     desc = (None, pymongo.DESCENDING, pymongo.ASCENDING)
@@ -61,16 +71,66 @@ def options():
 
 class TestClientStore:
 
-    def test_add_update_delete(self, postgres_connection:DatabaseConnection, sql_connection: DatabaseConnection, mongo_connection:DatabaseConnection):
-        pass
+    def test_add_get_update_delete_postgres(self, postgres_connection:DatabaseConnection, test_client: Client):
+        self.helper_add_get_update_delete(postgres_connection, test_client)
+    
+    def test_add_get_update_delete_sqlite(self, sql_connection:DatabaseConnection, test_client: Client):
+        self.helper_add_get_update_delete(sql_connection, test_client)
+    
+    def test_add_get_update_delete_mongo(self, mongo_connection:DatabaseConnection, test_client: Client):
+        self.helper_add_get_update_delete(mongo_connection, test_client)
 
-    def test_list(self, db_connections_with_data: list[tuple[str, DatabaseConnection]], options: list[tuple]):   
+    def helper_add_get_update_delete(self, db:DatabaseConnection, test_client: Client): 
+        # Add a client and check that we get the added client back
+        success, read_client1 = db.client_store.add(test_client)
+        assert success == True
+        assert isinstance(read_client1.client_id, str)
+        read_client1_dict = read_client1.to_dict()
+        client_id = read_client1_dict["client_id"]
+        del read_client1_dict["client_id"]
+        assert read_client1_dict == test_client.to_dict()
+
+        # Assert we get the same client back
+        read_client2 = db.client_store.get(client_id)
+        assert read_client2 is not None
+        assert read_client2.to_dict() == read_client1.to_dict()
+        
+        # Update the client and check that we get the updated client back
+        read_client2.name = "new_name"         
+        success, read_client3 = db.client_store.update(read_client2)
+        assert success == True
+        assert read_client3.name == "new_name"
+
+        # Assert we get the same client back
+        read_client4 = db.client_store.get(client_id)
+        assert read_client4 is not None
+        assert read_client3.to_dict() == read_client4.to_dict()
+
+        # Partial update the client and check that we get the updated client back
+        update_client = Client(client_id=client_id, combiner="new_combiner")
+        success, read_client5 = db.client_store.update(update_client)
+        assert success == True
+        assert read_client5.combiner == "new_combiner"
+
+        # Assert we get the same client back
+        read_client6 = db.client_store.get(client_id)
+        assert read_client6 is not None
+        assert read_client6.to_dict() == read_client5.to_dict()
+
+        # Delete the client and check that it is deleted
+        success = db.client_store.delete(client_id)
+        assert success == True
+
+    def test_list_count(self, db_connections_with_data: list[tuple[str, DatabaseConnection]], options: list[tuple]):   
         for (name1, db_1), (name2, db_2) in zip(db_connections_with_data[1:], db_connections_with_data[:-1]):
             print("Running tests between databases {} and {}".format(name1, name2))
             for *opt,kwargs in options:                        
                 gathered_clients = db_1.client_store.select(*opt, **kwargs)
                 gathered_clients2 = db_2.client_store.select(*opt, **kwargs)
-                
+                count = db_1.client_store.count(**kwargs)
+                count2 = db_2.client_store.count(**kwargs)
+
+                assert count == count2
                 assert len(gathered_clients) == len(gathered_clients2)
 
                 for i in range(len(gathered_clients)):
