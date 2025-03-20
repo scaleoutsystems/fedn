2d4380ca300eb71448a4942184b086a11e0c66cf
Viktor Valadi <42983197+viktorvaladi@users.noreply.github.com>
2025-03-20 17:21:43 +0100

    Fix/SK-1499 | Minor docs and example code update to Server functions (#848)
    * minor updates
    
    * Update start_sf_session.py
    
    * Update README.rst

--- examples/server-functions/start_sf_session.py
+++ examples/server-functions/start_sf_session.py
@@ -2,6 +2,7 @@ from server_functions import ServerFunctions
 
 from fedn import APIClient
 
+# Fetch your host address from the studio UI and add it below.
 client = APIClient(host="", secure=True, verify=True)
 
 print(client.start_session(server_functions=ServerFunctions, helper="numpyhelper"))
