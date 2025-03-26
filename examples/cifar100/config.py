31ff3c4ff897ae4f89172cb1e0c25c8d09ad5f5a
Andreas Hellander <andreas@scaleoutsystems.com>
2025-03-26 16:17:35 +0100

    Feature/SK-1257 | Bugfixes and improvements of combiner, and new load test (#774)

---
+++ examples/cifar100/config.py
@@ -0,0 +1,13 @@
+settings = {
+    "N_SPLITS": 20,
+    "DISCOVER_HOST": "api-server",
+    "DISCOVER_PORT": 8092,
+    "SECURE": False,
+    "VERIFY": False,
+    "ADMIN_TOKEN": None,
+    "CLIENT_TOKEN": None,
+    "BATCH_SIZE": 128,
+    "EPOCHS": 1,
+    "BALANCED": True,
+    "IID": True,
+}
