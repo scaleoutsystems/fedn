a8b2e953767022de6086988db3bb863926ee2402
viktorvaladi <viktor.valadi@hotmail.se>
2025-03-28 13:44:35 +0100

    server functions local test

--- fedn/network/combiner/hooks/serverfunctionsbase.py
+++ fedn/network/combiner/hooks/serverfunctionsbase.py
@@ -15,22 +15,16 @@ class ServerFunctionsBase(ABC):
         """
         pass
 
-    def aggregate(self, previous_global: List[np.ndarray], client_updates: Dict[str, Tuple[List[np.ndarray], Dict]]) -> List[np.ndarray]:
-        """Aggregates a list of parameters from clients.
+    def client_selection(self, client_ids: List[str]) -> List:
+        """Returns a list of client_id's of which clients to be used for the next training request.
 
         Args:
         ----
-            previous_global (list[np.ndarray]): A list of parameters representing the global
-            model from the previous round.
-
-            client_updates (list[list[np.ndarray]]): A dictionary where the key is client ID,
-            pointing to a tuple with the first element being client parameter and second element
-            being the clients metadata.
+            client_ids (list[str]): A list of client_ids for all connected clients.
 
         Returns:
         -------
-            list[np.ndarray]: A list of numpy arrays representing the aggregated
-            parameters across all clients.
+            list[str]: A list of client ids for which clients should be chosen for the next training round.
 
         """
         pass
@@ -51,16 +45,22 @@ class ServerFunctionsBase(ABC):
         """
         pass
 
-    def client_selection(self, client_ids: List[str]) -> List:
-        """Returns a list of client_id's of which clients to be used for the next training request.
+    def aggregate(self, previous_global: List[np.ndarray], client_updates: Dict[str, Tuple[List[np.ndarray], Dict]]) -> List[np.ndarray]:
+        """Aggregates a list of parameters from clients.
 
         Args:
         ----
-            client_ids (list[str]): A list of client_ids for all connected clients.
+            previous_global (list[np.ndarray]): A list of parameters representing the global
+            model from the previous round.
+
+            client_updates (list[list[np.ndarray]]): A dictionary where the key is client ID,
+            pointing to a tuple with the first element being client parameter and second element
+            being the clients metadata.
 
         Returns:
         -------
-            list[str]: A list of client ids for which clients should be chosen for the next training round.
+            list[np.ndarray]: A list of numpy arrays representing the aggregated
+            parameters across all clients.
 
         """
         pass
