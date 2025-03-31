03dcc574d39f1e86bec4c9f487fbe3b73703cd7d
Niklas <niklas@scaleoutsystems.com>
2025-03-31 18:11:26 +0200

    Bug/SK-1517 | DTO finding (#863)

--- fedn/network/storage/statestore/stores/validation_store.py
+++ fedn/network/storage/statestore/stores/validation_store.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict
 
 import pymongo
 from pymongo.database import Database
@@ -12,32 +12,9 @@ class ValidationStore(Store[ValidationDTO]):
     pass
 
 
-def translate_key_mongo(key: str):
-    if key == "correlation_id":
-        key = "correlationId"
-    elif key == "model_id":
-        key = "modelId"
-    elif key == "session_id":
-        key = "sessionId"
-
-    return key
-
-
 class MongoDBValidationStore(ValidationStore, MongoDBStore[ValidationDTO]):
     def __init__(self, database: Database, collection: str):
         super().__init__(database, collection, "validation_id")
-
-    def list(self, limit: int, skip: int, sort_key: str, sort_order=pymongo.DESCENDING, **kwargs) -> List[ValidationDTO]:
-        items = super().list(limit, skip, sort_key, sort_order, **kwargs)
-        _kwargs = {translate_key_mongo(k): v for k, v in kwargs.items()}
-        _sort_key = translate_key_mongo(sort_key)
-        if _kwargs != kwargs or _sort_key != sort_key:
-            items = super().list(limit, skip, _sort_key, sort_order, **_kwargs) + items
-        return items
-
-    def count(self, **kwargs) -> int:
-        kwargs = {translate_key_mongo(k): v for k, v in kwargs.items()}
-        return super().count(**kwargs)
 
     def _document_from_dto(self, item: ValidationDTO) -> Dict:
         item_dict = item.to_db(exclude_unset=False)
@@ -45,13 +22,6 @@ class MongoDBValidationStore(ValidationStore, MongoDBStore[ValidationDTO]):
 
     def _dto_from_document(self, document: Dict) -> ValidationDTO:
         dto_dict = from_document(document)
-
-        if "correlationId" in dto_dict:
-            dto_dict["correlation_id"] = dto_dict["correlationId"]
-        if "modelId" in dto_dict:
-            dto_dict["model_id"] = dto_dict["modelId"]
-        if "sessionId" in dto_dict:
-            dto_dict["session_id"] = dto_dict["sessionId"]
 
         return ValidationDTO().patch_with(dto_dict, throw_on_extra_keys=False, verify=True)
 
@@ -67,12 +37,6 @@ def _translate_key_sql(key: str) -> str:
         key = "receiver_name"
     elif key == "receiver.role":
         key = "receiver_role"
-    elif key == "correlationId":
-        key = "correlation_id"
-    elif key == "modelId":
-        key = "model_id"
-    elif key == "sessionId":
-        key = "session_id"
 
     return key
 
