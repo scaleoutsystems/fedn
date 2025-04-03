9223f18fb556cbb7e02160b9ff1b3e9636431d3a
Viktor Valadi <42983197+viktorvaladi@users.noreply.github.com>
2025-04-03 14:06:20 +0200

    Feature/SK-1512 | Test server functions locally (#860)
    * feature/SK-1512
    
    * copilot comments

---
+++ examples/server-functions/test_locally.py
@@ -0,0 +1,10 @@
+"""Helper function to test if your server functions implementation runs correctly."""
+from client.model import compile_model
+from fedn.network.combiner.hooks.serverfunctionstest import test_server_functions
+from server_functions import ServerFunctions
+
+
+model = compile_model()
+parameters_np = [val.cpu().numpy() for _, val in model.state_dict().items()]
+client_metadata = {"num_examples": 1}
+test_server_functions(ServerFunctions, parameters_np, client_metadata, rounds=5, num_clients=20)
