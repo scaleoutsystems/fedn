9716e68e28386666997a7937d984b62d82def3fb
Fredrik Wrede <fredrik@scaleoutsystems.com>
2025-04-04 16:43:47 +0200

    Bug/SK-1529 | save_metric fails on cli client

--- fedn/network/clients/client_v2.py
+++ fedn/network/clients/client_v2.py
@@ -211,7 +211,7 @@ class Client:
             outpath = get_tmp_path()
             self.fedn_client.dispatcher.run_cmd(f"predict {inpath} {outpath}")
 
-            with open(outpath, "rb") as fh:
+            with open(outpath, "r") as fh:
                 metrics = json.load(fh)
 
             os.unlink(inpath)
