098d1af75eb82cf2f20c8ae1c9cdfc4fdce1436a
Carl Andersson <carl@scaleoutsystems.com>
2025-04-11 16:48:04 +0200

    Added log_attributes function + grpc calls

--- fedn/network/clients/fedn_client.py
+++ fedn/network/clients/fedn_client.py
@@ -155,6 +155,8 @@ class FednClient:
             if response.status_code == HTTP_STATUS_OK:
                 logger.info("Connect to FEDn Api - Client assigned to controller")
                 json_response = response.json()
+                self.set_client_id(json_response["client_id"])
+                self.set_name(json.get("name", json_response["client_id"]))
                 combiner_config = GrpcConnectionOptions.from_dict(json_response)
                 return ConnectToApiResult.Assigned, combiner_config
 
@@ -441,6 +443,27 @@ class FednClient:
         )
 
         return self.grpc_handler.send_model_metric(message)
+
+    def log_attributes(self, attributes: dict) -> bool:
+        """Log the attributes to the server.
+
+        Args:
+            attributes (dict): The attributes to log.
+
+        Returns:
+            bool: True if the attributes were logged successfully, False otherwise.
+
+        """
+        message = fedn.AttributeMessage()
+        message.sender.name = self.name
+        message.sender.client_id = self.client_id
+        message.sender.role = fedn.Role.CLIENT
+        message.timestamp.GetCurrentTime()
+
+        for key, value in attributes.items():
+            message.attributes.add(key=key, value=value)
+
+        return self.grpc_handler.send_attributes(message)
 
     def create_update_message(self, model_id: str, model_update_id: str, meta: dict, request: fedn.TaskRequest) -> fedn.ModelUpdate:
         """Create an update message."""
