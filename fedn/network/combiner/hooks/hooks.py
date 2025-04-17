9632f340ba3ac1f1b6932305f109f44b4bc70f35
viktorvaladi <viktor.valadi@hotmail.se>
2025-04-17 12:09:33 +0200

    add new functionality

--- fedn/network/combiner/hooks/hooks.py
+++ fedn/network/combiner/hooks/hooks.py
@@ -43,11 +43,14 @@ class FunctionServiceServicer(rpc.FunctionServiceServicer):
         :return: the client config response
         :rtype: :class:`fedn.network.grpc.fedn_pb2.ClientConfigResponse`
         """
-        logger.info("Received client config request.")
-        model, _ = unpack_model(request_iterator, self.helper)
-        client_settings = self.server_functions.client_settings(global_model=model)
-        logger.info(f"Client config response: {client_settings}")
-        return fedn.ClientConfigResponse(client_settings=json.dumps(client_settings))
+        try:
+            logger.info("Received client config request.")
+            model, _ = unpack_model(request_iterator, self.helper)
+            client_settings = self.server_functions.client_settings(global_model=model)
+            logger.info(f"Client config response: {client_settings}")
+            return fedn.ClientConfigResponse(client_settings=json.dumps(client_settings))
+        except Exception as e:
+            logger.exception("Error handling client config request: %s", e)
 
     def HandleClientSelection(self, request: fedn.ClientSelectionRequest, context):
         """Handle client selection from user defined code.
@@ -59,11 +62,14 @@ class FunctionServiceServicer(rpc.FunctionServiceServicer):
         :return: the client selection response
         :rtype: :class:`fedn.network.grpc.fedn_pb2.ClientSelectionResponse`
         """
-        logger.info("Received client selection request.")
-        client_ids = json.loads(request.client_ids)
-        client_ids = self.server_functions.client_selection(client_ids)
-        logger.info(f"Clients selected: {client_ids}")
-        return fedn.ClientSelectionResponse(client_ids=json.dumps(client_ids))
+        try:
+            logger.info("Received client selection request.")
+            client_ids = json.loads(request.client_ids)
+            client_ids = self.server_functions.client_selection(client_ids)
+            logger.info(f"Clients selected: {client_ids}")
+            return fedn.ClientSelectionResponse(client_ids=json.dumps(client_ids))
+        except Exception as e:
+            logger.exception("Error handling client selection request: %s", e)
 
     def HandleMetadata(self, request: fedn.ClientMetaRequest, context):
         """Store client metadata from a request.
@@ -75,24 +81,43 @@ class FunctionServiceServicer(rpc.FunctionServiceServicer):
         :return: the client meta response
         :rtype: :class:`fedn.network.grpc.fedn_pb2.ClientMetaResponse`
         """
-        logger.info("Received metadata")
-        client_id = request.client_id
-        metadata = json.loads(request.metadata)
-        # dictionary contains: [model, client_metadata] in that order for each key
-        self.client_updates[client_id] = self.client_updates.get(client_id, []) + [metadata]
-        return fedn.ClientMetaResponse(status="Metadata stored")
+        try:
+            logger.info("Received metadata")
+            client_id = request.client_id
+            metadata = json.loads(request.metadata)
+            # dictionary contains: [model, client_metadata] in that order for each key
+            self.client_updates[client_id] = self.client_updates.get(client_id, []) + [metadata]
+            self.check_running_aggregate(client_id)
+            return fedn.ClientMetaResponse(status="Metadata stored")
+        except Exception as e:
+            logger.exception("Error handling store metadata request: %s", e)
 
     def HandleStoreModel(self, request_iterator, context):
-        model, final_request = unpack_model(request_iterator, self.helper)
-        client_id = final_request.id
+        try:
+            model, final_request = unpack_model(request_iterator, self.helper)
+            client_id = final_request.id
+            if client_id == "global_model":
+                logger.info("Received previous global model")
+                self.previous_global = model
+            else:
+                logger.info(f"Received client model from client {client_id}")
+                # dictionary contains: [model, client_metadata] in that order for each key
+                self.client_updates[client_id] = [model] + self.client_updates.get(client_id, [])
+            self.check_running_aggregate(client_id)
+            return fedn.StoreModelResponse(status=f"Received model originating from {client_id}")
+        except Exception as e:
+            logger.exception("Error handling store model request: %s", e)
+
+    def check_running_aggregate(self, client_id):
+        # incremental aggregation (memory secure)
         if client_id == "global_model":
-            logger.info("Received previous global model")
-            self.previous_global = model
-        else:
-            logger.info(f"Received client model from client {client_id}")
-            # dictionary contains: [model, client_metadata] in that order for each key
-            self.client_updates[client_id] = [model] + self.client_updates.get(client_id, [])
-        return fedn.StoreModelResponse(status=f"Received model originating from {client_id}")
+            return
+        model_and_metadata_received = len(self.client_updates[client_id]) == 2
+        if model_and_metadata_received and self.implemented_functions["running_aggregate"]:
+            client_model = self.client_updates[client_id][0]
+            client_metadata = self.client_updates[client_id][1]
+            self.server_functions.running_aggregate(client_id, client_model, client_metadata, self.previous_global)
+            del self.client_updates[client_id]
 
     def HandleAggregation(self, request, context):
         """Receive and store models and aggregate based on user-defined code when specified in the request.
@@ -104,15 +129,22 @@ class FunctionServiceServicer(rpc.FunctionServiceServicer):
         :return: the aggregation response (aggregated model or None)
         :rtype: :class:`fedn.network.grpc.fedn_pb2.AggregationResponse`
         """
-        logger.info(f"Receieved aggregation request: {request.aggregate}")
-        aggregated_model = self.server_functions.aggregate(self.previous_global, self.client_updates)
-        model_bytesIO = model_as_bytesIO(aggregated_model, self.helper)
-        request_function = fedn.AggregationResponse
-        self.client_updates = {}
-        logger.info("Returning aggregate model.")
-        response_generator = bytesIO_request_generator(mdl=model_bytesIO, request_function=request_function, args={})
-        for response in response_generator:
-            yield response
+        try:
+            logger.info(f"Receieved aggregation request: {request.aggregate}")
+            if self.implemented_functions["running_aggregate"]:
+                aggregated_model = self.server_functions.get_running_aggregate_model()
+            else:
+                aggregated_model = self.server_functions.aggregate(self.previous_global, self.client_updates)
+
+            model_bytesIO = model_as_bytesIO(aggregated_model, self.helper)
+            request_function = fedn.AggregationResponse
+            self.client_updates = {}
+            logger.info("Returning aggregate model.")
+            response_generator = bytesIO_request_generator(mdl=model_bytesIO, request_function=request_function, args={})
+            for response in response_generator:
+                yield response
+        except Exception as e:
+            logger.exception("Error handling aggregation request: %s", e)
 
     def HandleProvidedFunctions(self, request: fedn.ProvidedFunctionsResponse, context):
         """Handles the 'provided_functions' request. Sends back which functions are available.
@@ -124,50 +156,30 @@ class FunctionServiceServicer(rpc.FunctionServiceServicer):
         :return: dict with str -> bool for which functions are available
         :rtype: :class:`fedn.network.grpc.fedn_pb2.ProvidedFunctionsResponse`
         """
-        logger.info("Receieved provided functions request.")
-        server_functions_code = request.function_code
-        # if no new code return previous
-        if server_functions_code == self.server_functions_code:
-            logger.info("No new server function code provided.")
-            logger.info(f"Provided functions: {self.implemented_functions}")
+        try:
+            logger.info("Receieved provided functions request.")
+            server_functions_code = request.function_code
+            # if no new code return previous
+            if server_functions_code == self.server_functions_code:
+                logger.info("No new server function code provided.")
+                logger.info(f"Provided functions: {self.implemented_functions}")
+                return fedn.ProvidedFunctionsResponse(available_functions=self.implemented_functions)
+
+            self.server_functions_code = server_functions_code
+            self.implemented_functions = {}
+            self._instansiate_server_functions_code()
+            functions = ["client_selection", "client_settings", "aggregate", "running_aggregate"]
+            for func in functions:
+                if func in self.server_functions_code:
+                    logger.info(f"String {func} found in server functions code, assuming function is implemented.")
+                    self.implemented_functions[func] = True
+                else:
+                    logger.info(f"No {func} found in server functions code.")
+                    self.implemented_functions[func] = False
+            logger.info(f"Provided function: {self.implemented_functions}")
             return fedn.ProvidedFunctionsResponse(available_functions=self.implemented_functions)
-
-        self.server_functions_code = server_functions_code
-        self.implemented_functions = {}
-        self._instansiate_server_functions_code()
-        # We are not sending dummy values here since the implementation might depend on model shape / implementations
-
-        # Implemented=False if return is None (indicating base implementation)
-
-        # check if aggregation is available
-        try:
-            ret = self.server_functions.aggregate(0, 0)
-            if ret is None:
-                self.implemented_functions["aggregate"] = False
-            else:
-                self.implemented_functions["aggregate"] = True
-        except Exception:
-            self.implemented_functions["aggregate"] = True
-        # check if client_settings is available
-        try:
-            ret = self.server_functions.client_settings(0)
-            if ret is None:
-                self.implemented_functions["client_settings"] = False
-            else:
-                self.implemented_functions["client_settings"] = True
-        except Exception:
-            self.implemented_functions["client_settings"] = True
-        # check if client_selection is available
-        try:
-            ret = self.server_functions.client_selection(0)
-            if ret is None:
-                self.implemented_functions["client_selection"] = False
-            else:
-                self.implemented_functions["client_selection"] = True
-        except Exception:
-            self.implemented_functions["client_selection"] = True
-        logger.info(f"Provided function: {self.implemented_functions}")
-        return fedn.ProvidedFunctionsResponse(available_functions=self.implemented_functions)
+        except Exception as e:
+            logger.exception("Error handling provided functions request: %s", e)
 
     def _instansiate_server_functions_code(self):
         # this will create a new user defined instance of the ServerFunctions class.
@@ -183,7 +195,7 @@ class FunctionServiceServicer(rpc.FunctionServiceServicer):
 def serve():
     """Start the hooks service."""
     # Keepalive settings: these detect if the client is alive
-    KEEPALIVE_TIME_MS = 60 * 1000  # send keepalive ping every 60 seconds
+    KEEPALIVE_TIME_MS = 5 * 60 * 1000  # send keepalive ping every 5 minutes
     KEEPALIVE_TIMEOUT_MS = 20 * 1000  # wait 20 seconds for keepalive ping ack before considering connection dead
     MAX_CONNECTION_IDLE_MS = 5 * 60 * 1000  # max idle time before server terminates the connection (5 minutes)
     MAX_MESSAGE_LENGTH = 1 * 1024 * 1024 * 1024  # 1 GB in bytes
