b7e7b4bb21c2c752a9fd3cc7fda032ec1c78eec2
Sigvard Dackevall <sigvard@Sigvards-MacBook-Air.local>
2025-04-24 10:24:08 +0200

    changes to log handling and num workers to allow for 1000 clients connected

--- fedn/network/combiner/hooks/hooks.py
+++ fedn/network/combiner/hooks/hooks.py
@@ -200,7 +200,7 @@ def serve():
     MAX_CONNECTION_IDLE_MS = 5 * 60 * 1000  # max idle time before server terminates the connection (5 minutes)
     MAX_MESSAGE_LENGTH = 1 * 1024 * 1024 * 1024  # 1 GB in bytes
     server = grpc.server(
-        futures.ThreadPoolExecutor(max_workers=100),  # Increase based on expected load
+        futures.ThreadPoolExecutor(max_workers=10000),  # Increase based on expected load
         options=[
             ("grpc.keepalive_time_ms", KEEPALIVE_TIME_MS),
             ("grpc.keepalive_timeout_ms", KEEPALIVE_TIMEOUT_MS),
