6eaea2f94044292a8fa4b544f5b73f77d5ee78a7
Sigvard Dackevall <sigvard@Sigvards-MacBook-Air.local>
2025-04-29 15:27:53 +0200

    Updates to prolong keep alive timeout in conjunction with benjamins updates on branch: feature/SK-1353

--- fedn/network/grpc/server.py
+++ fedn/network/grpc/server.py
@@ -31,9 +31,9 @@ class Server:
         KEEPALIVE_TIMEOUT_MS = 30 * 1000
         # max idle time before server terminates the connection (5 minutes)
         MAX_CONNECTION_IDLE_MS = 5 * 60 * 1000
-
-        MAX_PING_STRIKES = 2
-
+        
+        HTTP2_MAX_PINGS_WITHOUT_DATA = 2  # limit clients to 2 pings without data
+        HTTP2_MIN_RECV_PING_INTERVAL_WITHOUT_DATA_MS = 10000  # require at least 10 seconds between client pings
         self.server = grpc.server(
             futures.ThreadPoolExecutor(max_workers=10000),
             interceptors=[JWTInterceptor()],
@@ -41,9 +41,9 @@ class Server:
                 ("grpc.keepalive_time_ms", KEEPALIVE_TIME_MS),
                 ("grpc.keepalive_timeout_ms", KEEPALIVE_TIMEOUT_MS),
                 ("grpc.max_connection_idle_ms", MAX_CONNECTION_IDLE_MS),
-                ("grpc.http2.max_pings_without_data", 0),  # Allow unlimited PINGs without data
-                ("grpc.http2.max_ping_strikes", MAX_PING_STRIKES),
-            ],
+                ("grpc.http2.max_pings_without_data", HTTP2_MAX_PINGS_WITHOUT_DATA),
+                 ("grpc.http2.min_recv_ping_interval_without_data_ms", HTTP2_MIN_RECV_PING_INTERVAL_WITHOUT_DATA_MS),
+             ],
         )
         self.certificate = None
         self.health_servicer = health.HealthServicer()
