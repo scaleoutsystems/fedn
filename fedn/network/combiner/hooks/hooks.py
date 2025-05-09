9d75e1c1246753408ad824f635ccf63f39940731
viktorvaladi <viktor.valadi@hotmail.se>
2025-05-09 11:36:56 +0200

    comment fix

--- fedn/network/combiner/hooks/hooks.py
+++ fedn/network/combiner/hooks/hooks.py
@@ -7,9 +7,9 @@ import grpc
 import fedn.network.grpc.fedn_pb2 as fedn
 import fedn.network.grpc.fedn_pb2_grpc as rpc
 from fedn.common.log_config import logger
-from fedn.network.combiner.hooks.allowed_import import *  # noqa: F403
 
-# imports for user code
+# imports for user defined code
+from fedn.network.combiner.hooks.allowed_import import *  # noqa: F403
 from fedn.network.combiner.hooks.allowed_import import ServerFunctionsBase
 from fedn.network.combiner.modelservice import bytesIO_request_generator, model_as_bytesIO, unpack_model
 from fedn.utils.helpers.plugins.numpyhelper import Helper
