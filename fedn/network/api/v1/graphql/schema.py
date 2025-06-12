c75e9a81a9297ad15fd67fdeb1c471c547c20b4f
Niklas <niklas@scaleoutsystems.com>
2025-06-12 13:42:22 +0200

    Feature/SK-1664 | Expand graphql schema (#947)

--- fedn/network/api/v1/graphql/schema.py
+++ fedn/network/api/v1/graphql/schema.py
@@ -4,6 +4,16 @@ from fedn.network.controller.control import Control
 from fedn.network.storage.statestore.stores.shared import SortOrder
 
 
+def get_sort_order_from_string(sort_order: str) -> SortOrder:
+    """Convert a string to a SortOrder enum."""
+    if sort_order.lower() == "asc":
+        return SortOrder.ASCENDING
+    elif sort_order.lower() == "desc":
+        return SortOrder.DESCENDING
+    else:
+        raise ValueError(f"Invalid sort order: {sort_order}")
+
+
 class ActorType(graphene.ObjectType):
     name = graphene.String()
     role = graphene.String()
@@ -12,25 +22,32 @@ class ActorType(graphene.ObjectType):
 class StatusType(graphene.ObjectType):
     data = graphene.String()
     extra = graphene.String()
-    id = graphene.String()
     logLevel = graphene.String()  # noqa: N815
     sender = graphene.Field(ActorType)
-    sessionId = graphene.String()  # noqa: N815
+    session_id = graphene.String()  # noqa: N815
     status = graphene.String()
     timestamp = graphene.String()
     type = graphene.String()
+    session = graphene.Field(lambda: SessionType)
+
+    def resolve_session(self, info):
+        db = Control.instance().db
+        session = db.session_store.get(self["session_id"])
+        if session:
+            return session.to_dict()
+        return None
 
 
 class ValidationType(graphene.ObjectType):
-    correlationId = graphene.String()  # noqa: N815
+    correlation_id = graphene.String()  # noqa: N815
     data = graphene.String()
-    id = graphene.String()
     meta = graphene.String()
-    modelId = graphene.String()  # noqa: N815
+    model_id = graphene.String()  # noqa: N815
     receiver = graphene.Field(ActorType)
     sender = graphene.Field(ActorType)
-    sessionId = graphene.String()  # noqa: N815
+    session_id = graphene.String()  # noqa: N815
     timestamp = graphene.String()
+    session = graphene.Field(lambda: SessionType)
 
     def resolve_receiver(self, info):
         return self["receiver"]
@@ -38,22 +55,44 @@ class ValidationType(graphene.ObjectType):
     def resolve_sender(self, info):
         return self["sender"]
 
+    def resolve_session(self, info):
+        db = Control.instance().db
+        session = db.session_store.get(self["session_id"])
+        if session:
+            return session.to_dict()
+        return None
+
 
 class ModelType(graphene.ObjectType):
-    id = graphene.String()
-    model = graphene.String()
     name = graphene.String()
     committed_at = graphene.DateTime()
+    model_id = graphene.String()
     session_id = graphene.String()
     parent_model = graphene.String()
-    validations = graphene.List(ValidationType)
+    validations = graphene.List(
+        ValidationType,
+        limit=graphene.Int(required=False, default_value=0),
+        skip=graphene.Int(required=False, default_value=0),
+        sort_key=graphene.String(required=False, default_value="committed_at"),
+        sort_order=graphene.String(required=False, default_value="desc"),
+    )
+    session = graphene.Field(lambda: SessionType)
 
-    def resolve_validations(self, info):
+    def resolve_validations(self, info, limit=0, skip=0, sort_key="committed_at", sort_order="desc"):
         db = Control.instance().db
-        kwargs = {"modelId": self["model"]}
-        result = db.validation_store.list(0, 0, None, sort_order=SortOrder.DESCENDING, **kwargs)
-        result = [validation.to_dict() for validation in result]
+        kwargs = {"model_id": self["model_id"]}
+        sort_order = get_sort_order_from_string(sort_order)
+
+        validations = db.validation_store.list(limit=limit, skip=skip, sort_key=sort_key, sort_order=sort_order, **kwargs)
+        result = [validation.to_dict() for validation in validations]
         return result
+
+    def resolve_session(self, info):
+        db = Control.instance().db
+        session = db.session_store.get(self["session_id"])
+        if session:
+            return session.to_dict()
+        return None
 
 
 class SessionConfigType(graphene.ObjectType):
@@ -70,39 +109,65 @@ class SessionConfigType(graphene.ObjectType):
 
 
 class SessionType(graphene.ObjectType):
-    id = graphene.String()
     session_id = graphene.String()
     name = graphene.String()
     committed_at = graphene.DateTime()
     session_config = graphene.Field(SessionConfigType)
-    models = graphene.List(ModelType)
-    validations = graphene.List(ValidationType)
-    statuses = graphene.List(StatusType)
+    models = graphene.List(
+        ModelType,
+        limit=graphene.Int(required=False, default_value=0),
+        skip=graphene.Int(required=False, default_value=0),
+        sort_key=graphene.String(required=False, default_value="committed_at"),
+        sort_order=graphene.String(required=False, default_value="desc"),
+    )
+    validations = graphene.List(
+        ValidationType,
+        limit=graphene.Int(required=False, default_value=0),
+        skip=graphene.Int(required=False, default_value=0),
+        sort_key=graphene.String(required=False, default_value="committed_at"),
+        sort_order=graphene.String(required=False, default_value="desc"),
+    )
+    statuses = graphene.List(
+        StatusType,
+        limit=graphene.Int(required=False, default_value=0),
+        skip=graphene.Int(required=False, default_value=0),
+        sort_key=graphene.String(required=False, default_value="committed_at"),
+        sort_order=graphene.String(required=False, default_value="desc"),
+    )
 
     def resolve_session_config(self, info):
         return self["session_config"]
 
-    def resolve_models(self, info):
+    def resolve_models(self, info, limit=0, skip=0, sort_key="committed_at", sort_order="desc"):
         db = Control.instance().db
+
         kwargs = {"session_id": self["session_id"]}
-        all_models = db.model_store.list(**kwargs)
-        result = [model.to_dict() for model in all_models]
+        sort_order = get_sort_order_from_string(sort_order)
+
+        models = db.model_store.list(limit=limit, skip=skip, sort_key=sort_key, sort_order=sort_order, **kwargs)
+        result = [model.to_dict() for model in models]
 
         return result
 
-    def resolve_validations(self, info):
+    def resolve_validations(self, info, limit=0, skip=0, sort_key="committed_at", sort_order="desc"):
         db = Control.instance().db
-        kwargs = {"sessionId": self["session_id"]}
-        result = db.validation_store.list(0, 0, None, sort_order=SortOrder.DESCENDING, **kwargs)
-        result = [validation.to_dict() for validation in result]
+
+        kwargs = {"session_id": self["session_id"]}
+        sort_order = get_sort_order_from_string(sort_order)
+
+        validations = db.validation_store.list(limit=limit, skip=skip, sort_key=sort_key, sort_order=sort_order, **kwargs)
+        result = [validation.to_dict() for validation in validations]
 
         return result
 
-    def resolve_statuses(self, info):
+    def resolve_statuses(self, info, limit=0, skip=0, sort_key="committed_at", sort_order="desc"):
         db = Control.instance().db
-        kwargs = {"sessionId": self["session_id"]}
-        result = db.status_store.list(0, 0, None, sort_order=SortOrder.DESCENDING, **kwargs)
-        result = [status.to_dict() for status in result]
+
+        kwargs = {"session_id": self["session_id"]}
+        sort_order = get_sort_order_from_string(sort_order)
+
+        statuses = db.status_store.list(limit=limit, skip=skip, sort_key=sort_key, sort_order=sort_order, **kwargs)
+        result = [status.to_dict() for status in statuses]
 
         return result
 
@@ -115,6 +180,10 @@ class Query(graphene.ObjectType):
     sessions = graphene.List(
         SessionType,
         name=graphene.String(),
+        limit=graphene.Int(required=False, default_value=25),
+        skip=graphene.Int(required=False, default_value=0),
+        sort_key=graphene.String(required=False, default_value="committed_at"),
+        sort_order=graphene.String(required=False, default_value="desc"),
     )
 
     model = graphene.Field(
@@ -125,6 +194,10 @@ class Query(graphene.ObjectType):
     models = graphene.List(
         ModelType,
         session_id=graphene.String(),
+        limit=graphene.Int(required=False, default_value=25),
+        skip=graphene.Int(required=False, default_value=0),
+        sort_key=graphene.String(required=False, default_value="committed_at"),
+        sort_order=graphene.String(required=False, default_value="desc"),
     )
 
     validation = graphene.Field(
@@ -135,6 +208,10 @@ class Query(graphene.ObjectType):
     validations = graphene.List(
         ValidationType,
         session_id=graphene.String(),
+        limit=graphene.Int(required=False, default_value=25),
+        skip=graphene.Int(required=False, default_value=0),
+        sort_key=graphene.String(required=False, default_value="committed_at"),
+        sort_order=graphene.String(required=False, default_value="desc"),
     )
 
     status = graphene.Field(
@@ -145,6 +222,10 @@ class Query(graphene.ObjectType):
     statuses = graphene.List(
         StatusType,
         session_id=graphene.String(),
+        limit=graphene.Int(required=False, default_value=25),
+        skip=graphene.Int(required=False, default_value=0),
+        sort_key=graphene.String(required=False, default_value="committed_at"),
+        sort_order=graphene.String(required=False, default_value="desc"),
     )
 
     def resolve_session(root, info, id: str = None):
@@ -153,14 +234,18 @@ class Query(graphene.ObjectType):
 
         return result.to_dict()
 
-    def resolve_sessions(root, info, name: str = None):
+    def resolve_sessions(root, info, name: str = None, limit: int = 25, skip: int = 0, sort_key: str = "committed_at", sort_order: str = "desc"):
         db = Control.instance().db
         if name:
             kwargs = {"name": name}
         else:
             kwargs = {}
-        all_sessions = db.session_store.list(**kwargs)
-        result = [session.to_dict() for session in all_sessions]
+
+        sort_order = get_sort_order_from_string(sort_order)
+
+        sessions = db.session_store.list(limit=limit, skip=skip, sort_key=sort_key, sort_order=sort_order, **kwargs)
+        result = [session.to_dict() for session in sessions]
+
         return result
 
     def resolve_model(root, info, id: str = None):
@@ -169,14 +254,18 @@ class Query(graphene.ObjectType):
 
         return result
 
-    def resolve_models(root, info, session_id: str = None):
+    def resolve_models(root, info, session_id: str = None, limit: int = 25, skip: int = 0, sort_key: str = "committed_at", sort_order: str = "desc"):
         db = Control.instance().db
         if session_id:
             kwargs = {"session_id": session_id}
         else:
             kwargs = {}
-        all_models = db.model_store.list(**kwargs)
-        result = [model.to_dict() for model in all_models]
+
+        sort_order = get_sort_order_from_string(sort_order)
+
+        models = db.model_store.list(limit=limit, skip=skip, sort_key=sort_key, sort_order=sort_order, **kwargs)
+        result = [model.to_dict() for model in models]
+
         return result
 
     def resolve_validation(root, info, id: str = None):
@@ -185,15 +274,20 @@ class Query(graphene.ObjectType):
 
         return result
 
-    def resolve_validations(root, info, session_id: str = None):
+    def resolve_validations(root, info, session_id: str = None, limit: int = 25, skip: int = 0, sort_key: str = "committed_at", sort_order: str = "desc"):
         db = Control.instance().db
+
         if session_id:
             kwargs = {"session_id": session_id}
-            result = db.validation_store.list(0, 0, None, sort_order=SortOrder.DESCENDING, **kwargs)
         else:
-            result = db.validation_store.list(0, 0, None)
+            kwargs = {}
 
-        return [validation.to_dict() for validation in result]
+        sort_order = get_sort_order_from_string(sort_order)
+
+        validations = db.validation_store.list(limit=limit, skip=skip, sort_key=sort_key, sort_order=sort_order, **kwargs)
+        result = [validation.to_dict() for validation in validations]
+
+        return result
 
     def resolve_status(root, info, id: str = None):
         db = Control.instance().db
@@ -201,15 +295,20 @@ class Query(graphene.ObjectType):
 
         return result
 
-    def resolve_statuses(root, info, session_id: str = None):
+    def resolve_statuses(root, info, session_id: str = None, limit: int = 25, skip: int = 0, sort_key: str = "committed_at", sort_order: str = "desc"):
         db = Control.instance().db
-        if session_id:
-            kwargs = {"sessionId": session_id}
-            result = db.status_store.list(0, 0, None, sort_order=SortOrder.DESCENDING, **kwargs)
-        else:
-            result = db.status_store.list(0, 0, None)
 
-        return [status.to_dict() for status in result]
+        if session_id:
+            kwargs = {"session_id": session_id}
+        else:
+            kwargs = {}
+
+        sort_order = get_sort_order_from_string(sort_order)
+
+        statuses = db.status_store.list(limit=limit, skip=skip, sort_key=sort_key, sort_order=sort_order, **kwargs)
+        result = [status.to_dict() for status in statuses]
+
+        return result
 
 
 schema = graphene.Schema(query=Query)
