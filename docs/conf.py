4f1d3ea4062e63c3169bf3b2934ea2a1d4b342b3
Niklas <niklas@scaleoutsystems.com>
2025-06-23 11:20:20 +0200

    bump fedn

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ project = "FEDn"
 author = "Scaleout Systems AB"
 
 # The full version, including alpha/beta/rc tags
-release = "0.29.0"
+release = "0.30.0"
 
 # Add any Sphinx extension module names here, as strings
 extensions = [
