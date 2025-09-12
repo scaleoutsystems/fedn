ccf357ba976b9b43ca13c59bcee5619e3033b210
Carl Andersson <carl@scaleoutsystems.com>
2025-09-12 15:47:52 +0200

    Spelling

--- fedn/network/clients/task_receiver.py
+++ fedn/network/clients/task_receiver.py
@@ -19,8 +19,8 @@ class Task:
         self.runner_thread = None
         self.lock = threading.Lock()
         self.status = fedn.TaskStatus.TASK_PENDING
-        self.interupted = False
-        self.interupted_reason = None
+        self.interrupted = False
+        self.interrupted_reason = None
         self.response = None
         self.correlation_id = request.correlation_id
         self.done = False
@@ -38,7 +38,7 @@ class TaskReceiver:
     def start(self):
         self._task_manager_thread = threading.Thread(
             target=self.run_task_polling,
-            name="TaskReciever",
+            name="TaskReceiver",
             daemon=True,
         )
         self._task_manager_thread.start()
@@ -53,16 +53,16 @@ class TaskReceiver:
         """
         if self.current_task is not None and self.current_task.runner_thread == threading.current_thread():
             with self.current_task.lock:
-                if self.current_task.interupted:
-                    raise StoppedException(self.current_task.interupted_reason)
+                if self.current_task.interrupted:
+                    raise StoppedException(self.current_task.interrupted_reason)
 
     def abort_current_task(self):
         if self.current_task is not None:
             with self.current_task.lock:
-                if not self.current_task.interupted:
-                    self.current_task.interupted = True
-                    self.current_task.interupted_reason = "Aborted by client"
-            logger.info("TaskReciever: Trying to abort current task.")
+                if not self.current_task.interrupted:
+                    self.current_task.interrupted = True
+                    self.current_task.interrupted_reason = "Aborted by client"
+            logger.info("TaskReceiver: Aborting current task... ")
 
     def run_task_polling(self):
         while True:
@@ -88,37 +88,37 @@ class TaskReceiver:
                             self.current_task = None
 
                 if report.status == fedn.TaskStatus.TASK_REQUEST_NEW:
-                    logger.debug("TaskReciever: Reporting: Polling for task")
+                    logger.debug("TaskReceiver: Reporting: Polling for task")
                 else:
-                    logger.debug("TaskReciever: Reporting: Task status %s", fedn.TaskStatus.Name(report.status))
+                    logger.debug("TaskReceiver: Reporting: Task status %s", fedn.TaskStatus.Name(report.status))
 
                 task_request: fedn.TaskRequest = self.client.grpc_handler.PollAndReport(report)
 
                 if task_request.correlation_id:
                     if self.current_task is not None:
                         if self.current_task.correlation_id == task_request.correlation_id:
-                            # Recieved update to current task
+                            # Received update to current task
                             if task_request.task_status == fedn.TaskStatus.TASK_INTERRUPTED:
-                                if not self.current_task.interupted:
+                                if not self.current_task.interrupted:
                                     with self.current_task.lock:
-                                        self.current_task.interupted = True
-                                        self.current_task.interupted_reason = "Aborted by server"
-                                    logger.info("TaskReciever: Recieved interupt message for task %s.", self.current_task.correlation_id)
+                                        self.current_task.interrupted = True
+                                        self.current_task.interrupted_reason = "Aborted by server"
+                                    logger.info("TaskReceiver: Recieved interupt message for task %s.", self.current_task.correlation_id)
                             elif task_request.task_status == fedn.TaskStatus.TASK_TIMEOUT:
-                                if not self.current_task.interupted:
+                                if not self.current_task.interrupted:
                                     with self.current_task.lock:
-                                        self.current_task.interupted = True
-                                        self.current_task.interupted_reason = "Timeout by server"
-                                    logger.info("TaskReciever: Recieved timeout message for task %s.", self.current_task.correlation_id)
+                                        self.current_task.interrupted = True
+                                        self.current_task.interrupted_reason = "Timeout by server"
+                                    logger.info("TaskReceiver: Recieved timeout message for task %s.", self.current_task.correlation_id)
                         else:
                             logger.warning(
-                                "TaskReciever: Received new task %s while processing task %s. Ignoring new task.",
+                                "TaskReceiver: Received new task %s while processing task %s. Ignoring new task.",
                                 task_request.correlation_id,
                                 self.current_task.correlation_id,
                             )
                     else:
                         # New task
-                        logger.info("TaskReciever: Got task %s", task_request.correlation_id)
+                        logger.info("TaskReceiver: Got task %s", task_request.correlation_id)
                         self.current_task = Task(task_request)
 
                         # Run the task in a separate thread
@@ -129,7 +129,7 @@ class TaskReceiver:
                 if toc - tic < self.polling_interval:
                     time.sleep(self.polling_interval - (toc - tic))
             except Exception as e:
-                logger.error("TaskReciever: Error in task polling: %s", e)
+                logger.error("TaskReceiver: Error in task polling: %s", e)
                 break
         self._task_manager_stop_event.set()
 
@@ -144,12 +144,12 @@ class TaskReceiver:
                 task.status = fedn.TaskStatus.TASK_COMPLETED
         except StoppedException as e:
             with task.lock:
-                logger.info("TaskReciever: Task interupted: %s", e)
+                logger.info("TaskReceiver: Task interupted: %s", e)
                 task.status = fedn.TaskStatus.TASK_INTERRUPTED
                 task.response = {"msg": str(e)}
         except Exception as e:
             with task.lock:
-                logger.error("TaskReciever: Task failed: %s", e)
+                logger.error("TaskReceiver: Task failed: %s", e)
                 task.status = fedn.TaskStatus.TASK_FAILED
                 task.response = {"error": str(e)}
         finally:
